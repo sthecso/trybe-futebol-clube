3d94182d7b0821a5c039725073b6510f08021af2
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-03-15 19:39:49 -0300

    Model Match createad

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,58 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Club from './Club';
+
+interface MatchAttributes {
+  id?: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+export default class Match extends Model<MatchAttributes> {}
+
+Match.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    homeTeam: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    homeTeamGoals: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    awayTeam: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    awayTeamGoals: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    inProgress: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    modelName: 'Match',
+    sequelize: db,
+    tableName: 'matchs',
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+Club.belongsTo(Match, { foreignKey: 'homeTeam', as: 'match' });
+Club.belongsTo(Match, { foreignKey: 'awayTeam', as: 'match' });
+
+Match.hasMany(Club, { foreignKey: 'homeTeam', as: 'homeTeam' });
+Match.hasMany(Club, { foreignKey: 'awayTeam', as: 'awayTeam' });
