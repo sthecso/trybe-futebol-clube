819839ac395a727110e2caec0c052ed89565743c
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-03-15 20:22:51 -0300

    Model User Created

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,46 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+interface UserAttributes {
+  id?: number;
+  name: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+export default class User extends Model<UserAttributes> {}
+
+User.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    role: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    modelName: 'User',
+    sequelize: db,
+    tableName: 'users',
+    timestamps: false,
+    underscored: true,
+  },
+);
