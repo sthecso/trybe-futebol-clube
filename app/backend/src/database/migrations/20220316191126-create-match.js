e30dc0b86485bbb83b8d139e3312dd44300afa93
Lanai Conceição <conceicaolanai@gmail.com>
2022-03-16 16:18:49 -0300

    feat: creates migration create-match

---
+++ app/backend/src/database/migrations/20220316191126-create-match.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matchs', {
+      id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate:'CASCADE',
+        onDelete:'CASCADE',
+        // Em 'references' avisamos de onde pegaremos os valores da coluna 'home_team'
+        references:{
+          model:'clubs',
+          key:'id',
+        },
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate:'CASCADE',
+        onDelete:'CASCADE',
+        // Em 'references' avisamos de onde pegaremos os valores da coluna 'away_team'
+        references:{
+          model:'clubs',
+          key:'id',
+        },
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      in_progress: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matchs')
+  }
+};
