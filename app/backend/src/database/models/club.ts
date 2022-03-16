f1fb95d5bc5e38003258bcebbd0c3e633214d10c
ArthurMGoulart <budiarthur@gmail.com>
2022-03-16 15:07:23 -0300

    feat: club model and migration

---
+++ app/backend/src/database/models/club.ts
@@ -0,0 +1,21 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Club extends Model {
+  public clubName: string;
+}
+Club.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  club_name: DataTypes.STRING,
+}, {
+  sequelize: db,
+  underscored: true,
+  modelName: 'Club',
+  timestamps: false,
+});
+
+export default Club;
