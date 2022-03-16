5a5f981e990fddc01ce3fef8119b9319f4ee03ef
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-03-15 22:11:49 -0300

    feat: criando models

---
+++ app/backend/src/database/models/matchs.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Matchs extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Matchs.init({
+    home_team: DataTypes.NUMBER,
+    home_team_goals: DataTypes.NUMBER,
+    away_team: DataTypes.NUMBER,
+    away_team_goals: DataTypes.NUMBER,
+    in_progress: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'matchs',
+    timestamps: false,
+  });
+  return Matchs;
+};
