5db07e1e436ff0919a9f81a5cdae5acd1f36cacf
ALINE <hoshinoaline@yahoo.com.br>
2022-03-15 23:34:45 -0300

    feat: implements model Match

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,60 @@
+import { Model, INTEGER } from 'sequelize';
+import db from '.';
+import Club from './Club';
+
+class Match extends Model {
+  public id: number;
+
+  public homeTeam: number;
+
+  public homeTeamGoals: number;
+
+  public awayTeam: number;
+
+  public awayTeamGoals: number;
+
+  public inProgress: number;
+}
+
+Match.init({
+  // ... Campo
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamgGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Match',
+  timestamps: false,
+});
+
+Club.belongsTo(Match, { foreignKey: 'homeTeam', as: 'match' });
+Club.belongsTo(Match, { foreignKey: 'awayTeam', as: 'match' });
+
+Match.hasMany(Club, { foreignKey: 'homeTeam', as: 'club' });
+Match.hasMany(Club, { foreignKey: 'awayTeam', as: 'club' });
+
+export default Match;
