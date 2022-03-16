2b701d56ba3baddcbdb031233ecb96f6009b6a85
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-03-15 22:27:33 -0300

    fix: login joi schema

--- app/backend/src/utils/joi.schemas.ts
+++ app/backend/src/utils/joi.schemas.ts
@@ -1,16 +1,17 @@
 import * as Joi from 'joi';
 
+const error = '401|All fields must be filled';
+
 export const login = Joi.object({
-  email: Joi.string().required().email().messages({
+  email: Joi.string().required().messages({
     'string.base': '422|Email must be a string',
-    'string.email': '422|Email must be a valid email address',
-    'string.empty': '400|Email is not allowed to be empty',
-    'any.required': '401|All fields must be filled',
+    'string.empty': error,
+    'any.required': error,
   }),
   password: Joi.string().required().messages({
     'string.base': '422|Password must be a string',
-    'string.empty': '400|Password is not allowed to be empty',
-    'any.required': '401|All fields must be filled',
+    'string.empty': error,
+    'any.required': error,
   }),
 });
 
