ca67caa7d7de86506bfd9ca119dda8ef9228031c
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-03-16 16:06:01 -0300

    test: arrumando teste de Token

--- app/backend/src/tests/auth_token.test.ts
+++ app/backend/src/tests/auth_token.test.ts
@@ -1,15 +1,25 @@
-import * as chai from 'chai';
-import { expect } from 'chai';
-import Token from '../auth/Token'
+import * as chai from "chai";
+import { expect } from "chai";
+import Token from "../auth/Token";
 
-describe('Teste Unitário Token', () => {
-  it('Testa se a função generate', () => {
-    const token = Token.generate({ email: 'test@test.com' })
-    expect(token).to.be.a('string')
-  })
+describe("Teste Unitário Token", () => {
 
-  it('Testa se a função verify', () => {
-    const payload = Token.verify(token)
-    expect(payload).to.be.a('object')
-  })
-})
+  const payload = {
+    sub: "1234567890",
+    name: "John Doe",
+    iat: 1516239022,
+  };
+
+  it("Testa se a função generate", () => {
+    const token = Token.generate(payload);
+    const result = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\
+.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ\
+.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    expect(result).to.equal(token);
+  });
+
+  it("Testa se a função verify", () => {
+    const result = Token.verify("token");
+    expect(payload).to.deep.equal(result);
+  });
+});
