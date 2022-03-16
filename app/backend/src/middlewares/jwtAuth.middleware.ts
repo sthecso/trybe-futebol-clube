12417e99145e81c9466ad487e869961528bd536f
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-03-16 15:09:46 -0300

    refactor: removed default JWT SECRET value

--- app/backend/src/middlewares/jwtAuth.middleware.ts
+++ app/backend/src/middlewares/jwtAuth.middleware.ts
@@ -2,14 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import { readFileSync } from 'fs';
 import * as jwt from 'jsonwebtoken';
 
-const SECRET = readFileSync('jwt.evaluation.key', { encoding: 'utf8' }) || 'teste';
-
 export default (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization;
 
   if (!token) return res.status(401).json({ message: 'Token not found' });
 
   try {
+    const SECRET = readFileSync('jwt.evaluation.key', { encoding: 'utf8' });
+
     const { id, email, role, username } = jwt.verify(token, SECRET) as jwt.JwtPayload;
 
     req.tokenData = { id, email, role, username };
