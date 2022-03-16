d45c3e02d93f2ba792be873163fef291e3ee25c5
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-03-16 15:09:21 -0300

    feat: add custom response to json syntax error

--- app/backend/src/middlewares/errorHandler.middleware.ts
+++ app/backend/src/middlewares/errorHandler.middleware.ts
@@ -3,5 +3,9 @@ import { NextFunction, Request, Response } from 'express';
 export default (error: unknown, _req: Request, res: Response, _next: NextFunction) => {
   console.log(error);
 
+  if (error instanceof SyntaxError && error.message.includes('Unexpected string in JSON')) {
+    return res.status(400).json({ message: 'Invalid body syntax' });
+  }
+
   return res.status(500).json({ message: 'Something went wrong here, please try again later' });
 };
