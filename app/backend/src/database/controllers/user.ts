ef23860758d2eef9738fdf738cc54036aeacc1a8
samuel <samuel.guilherme.l@hotmail.com>
2022-03-16 11:26:05 -0300

    feat: requisito 5

--- app/backend/src/database/controllers/user.ts
+++ app/backend/src/database/controllers/user.ts
@@ -4,12 +4,13 @@ import { RequestHandler } from 'express';
 import services from '../services/user';
 
 const getUser: RequestHandler = async (req, res) => {
-  const user = await services.getUser(req.body);
-  if (user === null) return res.status(400).json({ message: 'usuario nao encontrado' });
+  const userr = await services.getUser(req.body);
+  if (userr === null) return res.status(400).json({ message: 'usuario nao encontrado' });
   const { email, password } = req.body;
   const senha = await fs.readFile('../../../jwt.evaluation.key', 'utf8');
   const token = jwt.sign({ email, password }, senha, { expiresIn: '7D', algorithm: 'HS256' });
-  return res.status(201).json({ token });
+  const user = { userr };
+  return res.status(201).json({ user, token });
 };
 
 export default getUser;
--- app/backend/src/database/services/user.ts
+++ app/backend/src/database/services/user.ts
@@ -2,7 +2,9 @@ import { Login } from '../interfaces/login';
 import User from '../models/user';
 
 const getUser = async (login: Login) => {
-  const user = await User.findOne({ where: { email: login.email, password: login.password } });
+  const user = await User.findOne(
+    { where: { email: login.email, password: login.password }, attributes: ['password'] },
+  );
   if (user === null) return null;
   return user;
 };
