cc3648ab6010195680d2129f298ce14d68a63d40
ALINE <hoshinoaline@yahoo.com.br>
2022-03-17 19:40:49 -0300

    feat: fix email

--- app/backend/src/controller/middlewares/domain.ts
+++ app/backend/src/controller/middlewares/domain.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 
 export default (err:Error, req:Request, res:Response, _next:NextFunction) => {
   console.log(err.message, 'message');
-  if (err.message === 'Incorrect email or password') {
-    return res.status(401).json({ error: 'Incorrect email or password' });
+  if (err.message) {
+    return res.status(401).json({ message: err.message });
   }
   console.log(err.message);
   return res.status(500).json({ error: 'errei alguma coisa' });
--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -2,28 +2,23 @@ import * as bcrypt from 'bcryptjs';
 import * as Jwt from 'jsonwebtoken';
 import { readFileSync } from 'fs';
 import User from '../database/models/User';
-import { IUser } from '../interface/User';
 
 export default class ServiceLogin {
   public static async login(email:string, password:string) {
-    const user = await User.findOne({ where: { email } }) as unknown as IUser;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw new Error('Incorrect email or password');
+    }
     const bool = await bcrypt.compare(password, user.password);
-    if (!user || !bool) {
-      const error = new Error();
-      error.message = 'Incorrect email or password';
-      throw error;
+    if (!bool) {
+      throw new Error('Incorrect email or password');
     }
     const payload = {
       id: user.id,
     };
-
-    const jwtConfig:Jwt.SignOptions = {
-      expiresIn: '7d',
-      algorithm: 'HS256',
-    };
-    const {id, username, role } = user;
+    const { id, username, role } = user;
     const JWT_SECRET:Jwt.Secret = readFileSync('./jwt.evaluation.key', 'utf-8');
-    const token = Jwt.sign(payload, JWT_SECRET, jwtConfig);
+    const token = Jwt.sign(payload, JWT_SECRET);
     return { user: { id, username, role, email }, token };
   }
 }
