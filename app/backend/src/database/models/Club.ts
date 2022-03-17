0b4db78916e33799d5ba417b8c207097a19c458b
gesse_carlos <gesse.dev@outlook.com>
2022-03-17 00:10:57 -0300

    models finished

---
+++ app/backend/src/database/models/Club.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Model } from 'sequelize/types';
+import db from '.';
+
+export default class Club extends Model {
+  public id: number;
+
+  public clubName: string;
+}
+
+Club.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  clubName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+  modelName: 'Club',
+  tableName: 'clubs',
+});
---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,58 @@
+import { DataTypes, Model } from 'sequelize/types';
+import db from '.';
+import Club from './Club';
+
+export default class Match extends Model {
+  public id: number;
+
+  public homeTeam: number;
+
+  public homeTeamGoals: number;
+
+  public awayTeam: number;
+
+  public awayTeamGoals: number;
+
+  public inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+  modelName: 'Match',
+  tableName: 'matchs',
+});
+
+Club.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeMatch' });
+Club.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayMatch' });
+
+Match.belongsTo(Club, { foreignKey: 'homeTeam', as: 'homeClub' });
+Match.belongsTo(Club, { foreignKey: 'awayTeam', as: 'awayClub' });
