74b665f035a662a77238695958f89830afdafd8e
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-03-17 14:11:10 -0300

    fix: corrigindo o test do Token

--- app/backend/src/tests/auth_token.test.ts
+++ app/backend/src/tests/auth_token.test.ts
@@ -1,25 +1,22 @@
-import * as chai from "chai";
-import { expect } from "chai";
+import { assert, expect } from "chai";
 import Token from "../auth/Token";
 
 describe("Teste Unitário Token", () => {
-
   const payload = {
-    sub: "1234567890",
-    name: "John Doe",
-    iat: 1516239022,
+    "sub": "1234567890",
+    "name": "John Doe",
   };
 
-  it("Testa se a função generate", () => {
-    const token = Token.generate(payload);
-    const result = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\
-.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ\
-.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-    expect(result).to.equal(token);
+  it("Testa se a função generate existe", () => {
+       
+    assert.isFunction(Token.generate)
   });
 
   it("Testa se a função verify", () => {
-    const result = Token.verify("token");
-    expect(payload).to.deep.equal(result);
+    const token = Token.generate(payload)
+    const result = Token.verify(token);
+    console.log(result);
+    
+    expect(result).to.include(payload);
   });
 });
