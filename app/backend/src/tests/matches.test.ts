e3b53a8fc9f63ff72f40b335a3848e8322286a69
Murilo Rainho <murilorainho01@gmail.com>
2022-03-17 19:14:08 -0300

    [feat]: create tests to PUT http method '/match/:id' route

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -238,7 +238,8 @@ describe('POST \'/matchs\'', () => {
       .set('authorization', token)
       .send({
         ...mockSuccessBodyRequest,
-        awayTeam: 999,
+        awayTeam: 1,
+        homeTeam: 1,
       });
 
     const response = chaiHttpResponse.body;
@@ -337,6 +338,202 @@ describe('PATCH \'/matchs/:id/finish\'', () => {
     const response = chaiHttpResponse.body;
 
     expect(chaiHttpResponse.status).to.be.eql(200);
-    expect(response).to.be.eql('Finalizado');
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('Match was finish');
+  });
+});
+
+describe('PUT \'/matchs/:id\'', () => {
+  const mockSuccessBodyRequest = {
+    homeTeam: 11,
+    awayTeam: 7,
+    homeTeamGoals: 3,
+    awayTeamGoals: 1,
+    inProgress: true,
+  };
+
+  const mockSuccessBodyResponse = {
+    id: 1,
+    ...mockSuccessBodyRequest,
+  };
+
+  let token: string;
+
+  before(async () => {
+    shell.exec('npm run db:reset');
+
+    const { body: { token: loginToken } } = await chai
+      .request(app)
+      .post('/login')
+      .set('content-type', 'application/json')
+      .send({
+        email: 'admin@admin.com',
+        password: 'secret_admin',
+      })
+
+    token = loginToken;
+  });
+
+  after(() => {
+    shell.exec('npm run db:reset');
+  });
+
+  it('with an invalid \'homeTeam\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send({
+        ...mockSuccessBodyRequest,
+        homeTeam: 'ThisIsANumber',
+      });
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(401);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('\'homeTeam\' must be a number');
+  });
+
+  it('with an invalid \'awayTeam\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send({
+        ...mockSuccessBodyRequest,
+        awayTeam: 'ThisIsANumber',
+      });
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(401);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('\'awayTeam\' must be a number');
+  });
+
+  it('with an invalid \'homeTeamGoals\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send({
+        ...mockSuccessBodyRequest,
+        homeTeamGoals: 'ThisIsANumber',
+      });
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(401);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('\'homeTeamGoals\' must be a number');
+  });
+
+  it('with an invalid \'awayTeamGoals\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send({
+        ...mockSuccessBodyRequest,
+        awayTeamGoals: 'ThisIsANumber',
+      });
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(401);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('\'awayTeamGoals\' must be a number');
+  });
+
+  it('with an invalid \'inProgress\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send({
+        ...mockSuccessBodyRequest,
+        inProgress: 'ThisIsABoolean',
+      });
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(401);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('There is no team with such id!');
+  });
+
+  it('no team was found that has the id equal to \'homeTeam\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send({
+        ...mockSuccessBodyRequest,
+        homeTeam: 999,
+      });
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(401);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('There is no team with such id!');
+  });
+
+  it('no team was found that has the id equal to \'awayTeam\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send({
+        ...mockSuccessBodyRequest,
+        awayTeam: 999,
+      });
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(401);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('There is no team with such id!');
+  });
+
+  it('with \'homeTeam\' equal to \'awayTeam\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send({
+        ...mockSuccessBodyRequest,
+        awayTeam: 1,
+        homeTeam: 1,
+      });
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(401);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('There is no team with such id!');
+  });
+
+  it('on success', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .put('/matchs/1')
+      .set('content-type', 'application/json')
+      .set('authorization', token)
+      .send(mockSuccessBodyRequest);
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(200);
+    expect(response).to.deep.equal(mockSuccessBodyResponse);
   });
 });
