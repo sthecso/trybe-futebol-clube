10d5184d22982b0ef4330585c886610497ccf98b
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-17 14:26:49 -0300

    feat: import models on index.ts and export

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,5 +1,9 @@
 import { Sequelize } from 'sequelize';
+import User from './User';
+import Club from './Club';
+import Match from './Match';
 
 const databaseConfig = require('../config/database');
 
 export default new Sequelize(databaseConfig);
+export { User, Match, Club }
