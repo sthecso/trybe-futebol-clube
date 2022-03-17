d55176a56efb614fede631d662e435fa21ccefa0
Murilo Rainho <murilorainho01@gmail.com>
2022-03-17 12:10:42 -0300

    [feat]: create test to GET http method '/matchs' route, to test 'inProgress' query string

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -12,8 +12,49 @@ const { expect } = chai;
 
 let chaiHttpResponse;
 
+const matchesNotInProgress = [];
+
+const matchesInProgress = [];
+
 describe('/matchs', () => {
-  it('on success', async () => {
+  it('with an invalid \'inProgress\' query string', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matchs?inProgress=thisIsBoolean')
+      .set('content-type', 'application/json');
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(400);
+    expect(response).to.have.own.property('message');
+    expect(response.message).to.be.eql('\'inProgress\' must have \'true\' or \'false\'');
+  });
+
+  it('with \'inProgress\' query string equal to \'false\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matchs?inProgress=false')
+      .set('content-type', 'application/json');
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(200);
+    expect(response).to.be.eql(matchesNotInProgress);
+  });
+
+  it('with \'inProgress\' query string equal to \'true\'', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/matchs?inProgress=true')
+      .set('content-type', 'application/json');
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(200);
+    expect(response).to.be.eql(matchesInProgress);
+  });
+
+  it('on success without \'inProgress\' query string', async () => {
     chaiHttpResponse = await chai
       .request(app)
       .get('/matchs')
