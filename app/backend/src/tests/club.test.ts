d26eeb7bcf44b2ae490c5022a8a2a93439b2f869
Murilo Rainho <murilorainho01@gmail.com>
2022-03-17 09:22:36 -0300

    [feat]: create test to GET http method '/clubs' route (by id)

--- app/backend/src/tests/club.test.ts
+++ app/backend/src/tests/club.test.ts
@@ -13,7 +13,7 @@ const { expect } = chai;
 let chaiHttpResponse;
 
 describe('/clubs', () => {
-  it('has an correct body request', async () => {
+  it('on success', async () => {
     chaiHttpResponse = await chai
       .request(app)
       .get('/clubs')
@@ -23,5 +23,43 @@ describe('/clubs', () => {
 
     expect(chaiHttpResponse.status).to.be.eql(200);
     expect(response).to.be.eql(allClubsMock);
+  });
+});
+
+describe('/clubs/:id', () => {
+  it('has a invalid id', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/clubs/asdoaksdok')
+      .set('content-type', 'application/json');
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(400);
+    expect(response).to.have.own.property('message');
+  });
+
+  it('not found a club with a valid id', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/clubs/999')
+      .set('content-type', 'application/json');
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(404);
+    expect(response).to.have.own.property('message');
+  });
+
+  it('found a club with a valid id', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/clubs/2')
+      .set('content-type', 'application/json');
+
+    const response = chaiHttpResponse.body;
+
+    expect(chaiHttpResponse.status).to.be.eql(200);
+    expect(response).to.be.eql(allClubsMock[1]);
   });
 });
