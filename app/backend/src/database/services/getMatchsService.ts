2628e3867f050c2f5d0cd2c326e8577c6b6abda1
Victor Diniz <victoralbertocostadiniz@gmail.com>
2022-03-17 18:05:04 -0300

    Feat: endpoint /matchs desenvolvido completamente

--- app/backend/src/database/services/getMatchsService.ts
+++ app/backend/src/database/services/getMatchsService.ts
@@ -1,9 +1,25 @@
+import { Match } from '../../utils/Interfaces';
+import Clubs from '../models/Clubs';
 import Matchs from '../models/Matchs';
 
 async function getMatchsService() {
-  const matchs = await Matchs.findAll();
+  const matchs = await Matchs.findAll({
+    include: [
+      { model: Clubs, as: 'homeClub' },
+      { model: Clubs, as: 'awayClub' },
+    ],
+  });
 
-  return matchs;
+  const retypeMatchs = matchs as unknown as Match[]; // lint que mandou
+
+  const formatedMatchs = retypeMatchs.map((match: Match) => {
+    const { id, homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress } = match;
+    const homeClub = { clubName: match.homeClub.clubName };
+    const awayClub = { clubName: match.awayClub.clubName };
+    return { id, homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress, homeClub, awayClub };
+  });
+
+  return formatedMatchs;
 }
 
 export default getMatchsService;
