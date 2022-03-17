ebdf0a59fae1233d77380f9653dc650c49421aa6
matheushtc <matheushtc25@gmail.com>
2022-03-17 15:54:19 -0300

    Implementando testes para /login.

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,160 @@
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { Response } from 'superagent';
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('/login', () => {
+
+  describe('loginControler: ', () => {
+
+    describe('Login com sucesso: ', () => {
+      it('retorna um token', async () => {
+        const user = {
+          "email": "hello@word.com",
+          "password": "secret_admin"
+        }
+
+        await chai.request(app)
+          .post('/login')
+          .send(user)
+          .then((res: Response) => {
+            const { user, token } = res.body;
+
+            expect(res).to.have.status(200);
+            expect(token).to.be.a('string');
+            expect(user).to.be.a('object');
+            expect(token).not.to.be.undefined;
+            expect(user.email).to.be.equal("hello@word.com");
+          });
+      });
+
+
+    })
+    describe('Email: ', () => {
+      it('quando está vazio', async () => {
+        const user = {
+          email: '',
+          password: ''
+        };
+
+        await chai.request(app)
+          .post('/login')
+          .send(user)
+          .then((res: Response) => {
+            expect(res.status).to.equal(401);
+            expect(res.body.message).to.equal('All fields must be filled');
+          });
+        })
+
+        it('quando o email está incorreto', async () => {
+          const user = {
+            email: 'hello',
+            password: ''
+            };
+
+          await chai.request(app)
+            .post('/login')
+            .send(user)
+            .then((res: Response) => {
+              expect(res.status).to.equal(401);
+              expect(res.body.message).to.equal('Incorrect email or password');
+            });
+          })
+
+        it('quando não existe o campo email', async () => {
+          const user = {
+            password: ''
+            };
+
+          await chai.request(app)
+            .post('/login')
+            .send(user)
+            .then((res: Response) => {
+              expect(res.status).to.equal(401);
+              expect(res.body.message).to.equal('All fields must be filled');
+            });
+          })
+
+          it('quando email é um número', async () => {
+            const user = {
+              email: 1,
+              password: ''
+              };
+
+            await chai.request(app)
+              .post('/login')
+              .send(user)
+              .then((res: Response) => {
+                expect(res.status).to.equal(422);
+                expect(res.body.message).to.equal('email must be a string');
+              });
+            });
+    })
+
+    describe('Password: ', () => {
+      it('quando está vazio', async () => {
+        const user = {
+          email: 'hello@word.com',
+          password: ''
+        };
+
+        await chai.request(app)
+          .post('/login')
+          .send(user)
+          .then((res: Response) => {
+            expect(res.status).to.equal(401);
+            expect(res.body.message).to.equal('All fields must be filled');
+          });
+        })
+
+        it('quando password é incorreta', async () => {
+          const user = {
+            "email": "hello@word.com",
+            "password": ""
+          };
+
+          await chai.request(app)
+            .post('/login')
+            .send(user)
+            .then((res: Response) => {
+              expect(res.status).to.equal(401);
+              expect(res.body.message).to.equal('Incorrect email or password');
+            });
+        })
+
+        it('quando não existe o campo password', async () => {
+          const user = {
+            email: 'hello@word.com',
+          };
+
+          await chai.request(app)
+            .post('/login')
+            .send(user)
+            .then((res: Response) => {
+              expect(res.status).to.equal(401);
+              expect(res.body.message).to.equal('All fields must be filled');
+            });
+          })
+
+        it('quando é menor que 6 caracteres'), async () => {
+          const user = {
+            "email": "hello@word.com",
+            "password": "senha"
+          };
+
+          await chai.request(app)
+            .post('/login')
+            .send(user)
+            .then((res: Response) => {
+              expect(res.status).to.equal(401);
+              expect(res.body.message).to.equal('Incorrect email or password');
+            });
+        }
+      })
+    })
+})
