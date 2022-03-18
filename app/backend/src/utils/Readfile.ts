4a972c2eec1c2aaabd58acaf010cf23dfb687fb1
Filipe <filipecandido123@hotmail.com>
2022-03-18 15:12:10 -0300

    refactor: arquivo readFile.ts, mudado a forma de tratamento de leitura de assincrona para sincrona

--- app/backend/src/utils/Readfile.ts
+++ app/backend/src/utils/Readfile.ts
@@ -1,14 +1,10 @@
-import { readFile } from 'fs';
+import { readFileSync } from 'fs';
 import * as path from 'path';
 
 const filePath = path.join(__dirname, '../../jwt.evaluation.key');
 
 const readFileDocumentSecret = () => {
-  readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
-    if (err) throw err;
-    return (data);
-  });
-  return readFile;
+  const token = readFileSync(filePath, 'utf-8');
+  return token;
 };
-// console.log(readFileDocumentSecret());
 export default readFileDocumentSecret;
