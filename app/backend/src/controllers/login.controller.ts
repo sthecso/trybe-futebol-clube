0f09d0000958a8e502a0abbba17942d6b53cfca0
Josué Lobo <darksiders20005@gmail.com>
2022-03-18 13:02:05 -0300

    feat: controllerlogin implement

---
+++ app/backend/src/controllers/login.controller.ts
@@ -0,0 +1,13 @@
+import LoginService from '../services/login.service'
+import ICredentials from './interface/Credentials.interface';
+
+const loginService = new LoginService();
+
+export default class LoginController {
+  // public path: string = '/login';
+
+  public async create(data: ICredentials): Promise<any> {
+    const result = await loginService.create(data)
+    return result
+  }
+}
