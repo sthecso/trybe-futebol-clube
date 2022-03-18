7fb4a7b568d00ed6930d4bc8b9f1dac4cdd21b3a
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-18 10:43:25 -0300

    fix: erase imports

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,9 +1,5 @@
 import { Sequelize } from 'sequelize';
-import User from './User';
-import Club from './Club';
-import Match from './Match';
 
 const databaseConfig = require('../config/database');
 
 export default new Sequelize(databaseConfig);
-export { User, Match, Club }
