07640e8989c9c8c565df983dea5b9706f551be9e
marcos <marc0sdurval20@gmail.com>
2022-03-17 22:51:18 -0300

    feat: inicia testes

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,34 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+// import request from 'superagent';
+const PORT = process.env.PORT || 3001;
+
+const url = `http://localhost:${PORT}`
+chai.use(chaiHttp);
+// let chaiHttpResponse = Response
+const { expect } = chai
+// const rota = new App().start(3001)
+
+describe("testa o funcionamento da rota Login" ,() =>{
+    describe("testa o caso do login invalido" ,() =>{
+      it("email espera o erro 400" ,async()=>{
+        const result =  await chai.request(url)
+          .post('/login').send({
+           email: "batman",
+           password: "1234567",
+          })
+        expect(result).to.be.status(400)
+        expect(result.body).to.be.deep.equal({message: 'type email required'})
+      })
+      it("senha espera o erro 400" ,async()=>{
+        const result =  await chai.request(url)
+          .post('/login').send({
+           email: "batman@hotmail.com",
+           password: "12345",
+          })
+        expect(result).to.be.status(400)
+        expect(result.body).to.be.deep.equal({message: 'password must be longer than 6 characters'})
+      })
+    })
+})
