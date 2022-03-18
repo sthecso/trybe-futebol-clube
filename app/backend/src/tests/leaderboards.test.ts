9a398c6daff59b393ce35a06fee5d75a590dc20f
Murilo Rainho <murilorainho01@gmail.com>
2022-03-18 08:29:11 -0300

    [fix]: tests of GET http method '/leaderboard/home' route; fix GET http method '/leaderboard/home' route to return num AS INT instead of num AS VARCHAR

--- app/backend/src/tests/leaderboards.test.ts
+++ app/backend/src/tests/leaderboards.test.ts
@@ -4,13 +4,15 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 
+import {
+  leaderboardsNotInProgress,
+} from './mocks';
+
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 let chaiHttpResponse;
-
-const leaderboards = [];
 
 describe('GET \'/leaderboard/home\'', () => {
   it('on success', async () => {
@@ -22,6 +24,6 @@ describe('GET \'/leaderboard/home\'', () => {
     const response = chaiHttpResponse.body;
 
     expect(chaiHttpResponse.status).to.be.eql(200);
-    expect(response).to.be.eql(leaderboards);
+    expect(response).to.be.eql(leaderboardsNotInProgress);
   });
 });
--- app/backend/src/tests/mocks/index.ts
+++ app/backend/src/tests/mocks/index.ts
@@ -2,3 +2,4 @@ export { default as allClubsMock } from './allClubs';
 export { default as allMatchesMock } from './allMatches';
 export { default as matchesInProgress } from './matchesInProgress';
 export { default as matchesNotInProgress } from './matchesNotInProgress';
+export { default as leaderboardsNotInProgress } from './leaderboardsNotInProgress';
