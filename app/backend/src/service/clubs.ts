83c8ab599f3c4332e7bdf714eee64011abf6cbf9
rjanovicci <rjanovicci@hotmail.com>
2022-03-18 18:55:35 -0300

    feat: add clubs service (find functions)

---
+++ app/backend/src/service/clubs.ts
@@ -0,0 +1,23 @@
+import Clubs from '../database/models/ClubModel';
+
+interface IClub {
+  id: number,
+  clubName: string,
+}
+
+const findAllClubs = async () => {
+  const allClubs = await Clubs.findAll();
+
+  return allClubs;
+};
+
+const findClubById = async (searchId: number) => {
+  const foundClub = await Clubs.findOne({ where: { id: searchId } }) as IClub;
+
+  return foundClub;
+};
+
+export {
+  findAllClubs,
+  findClubById,
+};
