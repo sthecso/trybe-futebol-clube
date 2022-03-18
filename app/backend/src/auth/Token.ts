b734ceb5c35054d3041d79515c3e36b275f05216
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-03-17 21:24:33 -0300

    feat: add a função que lé chave secreta

--- app/backend/src/auth/Token.ts
+++ app/backend/src/auth/Token.ts
@@ -1,18 +1,23 @@
 import * as jwt from 'jsonwebtoken';
+import readFile from '../tools/readFile';
 
 class Token {
   private jwt;
-
+  private secretKey: string;
+  
   constructor(webtoken: any) {
     this.jwt = webtoken;
   }
-
+  
   async generate(payload: object): Promise<string> {
-    return this.jwt.sign(payload, '123654987');
+    this.secretKey = (await readFile('jwt.evaluation.key')).trim()
+    
+    return this.jwt.sign(payload, this.secretKey);
   }
-
+  
   async verify(token: string): Promise<object> {
-    return this.jwt.verify(token, '123654987');
+    this.secretKey = await readFile('jwt.evaluation.key')
+    return this.jwt.verify(token, this.secretKey);
   }
 }
 
