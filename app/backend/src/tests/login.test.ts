0fdaa6f34dcdbd21d30168f97ce1e9d20a53ba2d
rjanovicci <rjanovicci@hotmail.com>
2022-03-17 23:42:48 -0300

    test: add login.test

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,36 @@
+// import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+// import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Test endpoint /login', () => {
+  let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  it('status = 200 and returns an object', async () => {
+    chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({ email: '', password: '' })
+    
+    expect(chaiHttpResponse).have.status(401);
+  });
+});
