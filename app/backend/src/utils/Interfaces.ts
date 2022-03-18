7dce2d5929db0f142190f9654c5a868a70d9674f
Fernando Costa <fgosuendacosta@gmail.com>
2022-03-18 17:54:22 -0300

    refact: adjustments and refactoring interface

--- app/backend/src/utils/Interfaces.ts
+++ app/backend/src/utils/Interfaces.ts
@@ -1,36 +1,14 @@
-export interface User {
-  id: number,
-  username: string,
-  role: string,
-  email: string,
-}
-
-export interface UserWithPassword extends User {
-  password: string,
-}
-
-export interface LoginReturn {
-  user: User,
-  token: string
-}
-
-export interface TokenReturn {
-  data: UserWithPassword,
-  iat: number,
-  exp: number,
-}
-
-export interface Match {
-  id: number,
+export interface User { id: number, username: string, role: string, email: string }
+export interface UserWithPassword extends User { password: string }
+export interface LoginReturn { user: User, token: string }
+export interface TokenReturn { data: UserWithPassword, iat: number, exp: number }
+export interface Match extends MatchData { homeClub: ClubName, awayClub: ClubName }
+interface ClubName { clubName: string }
+export interface UserSentMatchData {
   homeTeam: number,
   homeTeamGoals: number,
   awayTeam: number,
   awayTeamGoals: number,
   inProgress: boolean,
-  homeClub: ClubName,
-  awayClub: ClubName
 }
-
-interface ClubName {
-  clubName: string
-}
+export interface MatchData extends UserSentMatchData { id: number }
