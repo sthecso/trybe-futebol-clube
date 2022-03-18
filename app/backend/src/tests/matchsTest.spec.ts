e9bc87aee973dfc4b7a077b6eddf32722776e35a
Victor Diniz <victoralbertocostadiniz@gmail.com>
2022-03-18 20:50:27 -0300

    Test: Completa testes da rota get patch e post /matchs

--- app/backend/src/tests/matchsTest.spec.ts
+++ app/backend/src/tests/matchsTest.spec.ts
@@ -25,6 +25,42 @@ describe('Testes da /matchs', () => {
     });
   });
 
+  describe('Quando o request é feito corretamente, na rota get passando valor inProgress true', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 200', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .get('/matchs?inProgress=true')
+  
+      expect(chaiHttpResponse.status).to.be.equal(200);
+    });
+  });
+
+  describe('Quando o request é feito corretamente, na rota get passando valor inProgress false', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 200', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .get('/matchs?inProgress=false')
+  
+      expect(chaiHttpResponse.status).to.be.equal(200);
+    });
+  });
+
+  describe('Quando o request é feito corretamente, na rota patch match/:id/finish passando id 1', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 200', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .patch('/matchs/1/finish')
+  
+      expect(chaiHttpResponse.status).to.be.equal(200);
+    });
+  });
+
   describe('Quando o request é feito corretamente, na rota post', () => {
     let chaiHttpResponse: Response;
   
@@ -43,6 +79,148 @@ describe('Testes da /matchs', () => {
          )
   
       expect(chaiHttpResponse.status).to.be.equal(201);
+    });
+  });
+
+  describe('Quando o request é feito incorretamente, na rota post, pois o homeTeam estava undefined', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 401', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/matchs')
+         .send(
+          {
+            "awayTeam": 8,
+            "homeTeamGoals": 2,
+            "awayTeamGoals": 2,
+            "inProgress": true
+          }
+         )
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+    });
+  });
+
+  describe('Quando o request é feito incorretamente, na rota post, pois o awayTeam estava undefined', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 401', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/matchs')
+         .send(
+          {
+            "homeTeam": 8,
+            "homeTeamGoals": 2,
+            "awayTeamGoals": 2,
+            "inProgress": true
+          }
+         )
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+    });
+  });
+
+  describe('Quando o request é feito incorretamente, na rota post, pois o homeTeamGoals estava undefined', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 401', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/matchs')
+         .send(
+          {
+            "homeTeam": 16,
+            "awayTeam": 8,
+            "awayTeamGoals": 2,
+            "inProgress": true
+          }
+         )
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+    });
+  });
+
+  describe('Quando o request é feito incorretamente, na rota post, pois o awayTeamGoals estava undefined', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 401', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/matchs')
+         .send(
+          {
+            "homeTeam": 16,
+            "awayTeam": 8,
+            "homeTeamGoals": 2,
+            "inProgress": true
+          }
+         )
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+    });
+  });
+
+  describe('Quando o request é feito incorretamente, na rota post, pois o inProgress estava undefined', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 401', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/matchs')
+         .send(
+          {
+            "homeTeam": 16,
+            "awayTeam": 8,
+            "homeTeamGoals": 5,
+            "awayTeamGoals": 2,
+          }
+         )
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+    });
+  });
+
+  describe('Quando o request é feito incorretamente, na rota post, pois os times eram iguais', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 401', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/matchs')
+         .send(
+          {
+            "homeTeam": 8,
+            "awayTeam": 8,
+            "homeTeamGoals": 5,
+            "awayTeamGoals": 2,
+            "inProgress": false,
+          }
+         )
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+    });
+  });
+
+  describe('Quando o request é feito incorretamente, na rota post, pois o time não existia', () => {
+    let chaiHttpResponse: Response;
+  
+    it('Retorna status 401', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .post('/matchs')
+         .send(
+          {
+            "homeTeam": 69,
+            "awayTeam": 8,
+            "homeTeamGoals": 5,
+            "awayTeamGoals": 2,
+            "inProgress": false,
+          }
+         )
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
     });
   });
 });
