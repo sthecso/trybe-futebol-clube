2c0f37f457b69bb11964a9d98dedc921a769e799
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-03-19 12:56:32 -0300

    fix: remove auth middleware from club route

--- app/backend/src/routes/club.ts
+++ app/backend/src/routes/club.ts
@@ -1,23 +1,16 @@
 import { Router } from 'express';
-import { auth } from '../middlewares';
 import { ClubController } from '../controllers';
 
 const clubs = Router();
 
 clubs.get(
   '/:id',
-  [
-    auth,
-    ClubController.findById,
-  ],
+  ClubController.findById,
 );
 
 clubs.get(
   '/',
-  [
-    auth,
-    ClubController.findAll,
-  ],
+  ClubController.findAll,
 );
 
 export default clubs;
