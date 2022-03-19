b9179f826df58266a86e28313b487bcf373e55c1
Josué Lobo <darksiders20005@gmail.com>
2022-03-18 22:42:13 -0300

    feat: implement password business rule

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,16 +1,39 @@
+import jwt from '../utils/jwt';
+import ICredentials from '../controllers/interface/Credentials.interface';
 import User from '../database/models/User';
+import Bcrypt from '../utils/bcryptjs';
 
 export default class LoginService {
-  async login(data: any): Promise<any> {
+  async login(data: ICredentials): Promise<any> {
     const user = await User.findOne({
       where: { email: data.email },
     });
     return user
   }
-  async create(data: any): Promise<any> {
+  async create(data: ICredentials): Promise<any> {
+    const { email, password } = data;
     const user = await User.findOne({
-      where: { email: data.email },
+      where: { email: email },
     });
-    return user
+    if (!user) {
+      const error = {
+        code: 401,
+        message: "Incorrect email or password",
+      };
+      return error;
+    }
+    const bcrypt = new Bcrypt(password, user.password);
+    const test = await bcrypt.decrypt()
+    if (!test) {
+      console.log("entrou ")
+      const error = {
+        code: 401,
+        message: "Incorrect email or password",
+      };
+      return error;
+    }
+    const { id } = user;
+    const token = jwt.sign({ email, id });
+    return { user, token };
   }
 }
