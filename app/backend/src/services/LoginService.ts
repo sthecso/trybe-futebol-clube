0f9bc4e9a8d2c5e56cdf7fd7a54dcaf694db07d7
devluizf <luizfurtadomkt@hotmail.com>
2022-03-19 16:28:23 -0300

    feat: cria o service de login

---
+++ app/backend/src/services/LoginService.ts
@@ -0,0 +1,43 @@
+import APIError from '../helpers/APIError';
+import User from '../database/models/User';
+import { sign, verify } from '../helpers/jwtHelper';
+import ILogin from '../interfaces/ILogin';
+import { compare } from '../helpers/bcryptHelper';
+import IUser from '../interfaces/IUser';
+
+class LoginService {
+  private UserModel = User;
+
+  private verifiedUser: IUser;
+
+  async login(data: ILogin) {
+    const user: IUser | null = await this.UserModel.findOne({
+      where: { email: data.email },
+      raw: true,
+    });
+    const error = new APIError('Incorrect email or password', 'unauthorized');
+
+    if (!user) throw error;
+
+    const userPassword = user.password as string;
+    const isPasswordCorrect = await compare(data.password, userPassword);
+    delete user.password;
+
+    if (!isPasswordCorrect) {
+      throw error;
+    }
+
+    const token = sign(user);
+    return {
+      user,
+      token,
+    };
+  }
+
+  validate(token: string) {
+    this.verifiedUser = verify(token) as IUser;
+    return this.verifiedUser.role;
+  }
+}
+
+export default LoginService;
