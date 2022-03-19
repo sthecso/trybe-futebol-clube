b616c0d9454d77f6504a85d8aafb3926227d3d76
Filipe <filipecandido123@hotmail.com>
2022-03-19 02:29:18 -0300

    refactor: model/user.ts atribuido allownull

--- app/backend/src/database/models/User.ts
+++ app/backend/src/database/models/User.ts
@@ -22,15 +22,20 @@ User.init({
   },
   username: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   role: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   email: {
     type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
   },
   password: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
 }, { sequelize: db, timestamps: false, modelName: 'user' });
 
