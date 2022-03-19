83d8eb951d1db0bbeb1444a2e5d509dc86d772e4
Lanai Conceição <conceicaolanai@gmail.com>
2022-03-19 09:27:32 -0300

    feat: creates login service

---
+++ app/backend/src/services/LoginService.ts
@@ -0,0 +1,29 @@
+import * as bcrypt from 'bcryptjs';
+import IUserResponse from '../interfaces/IUserResponse';
+import User from '../database/models/User';
+import { signToken } from '../utils/jwt';
+
+export default class LoginService {
+  public static async login(email: string, password: string) {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) throw new Error('Incorrect email or password. Try again!');
+
+    const comparePass = await bcrypt.compare(password, user.password);
+
+    if (!comparePass) throw new Error('Incorrect email or password. Try again!');
+
+    const payload: IUserResponse = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    };
+
+    const token = signToken(payload);
+
+    const { id, username, role } = user;
+
+    return { user: { id, username, role, email }, token };
+  }
+}
