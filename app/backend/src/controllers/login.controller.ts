e5177e7278ee414517256960cb10f538d05c3c24
Josué Lobo <darksiders20005@gmail.com>
2022-03-18 22:36:11 -0300

    refactor: linter

--- app/backend/src/controllers/login.controller.ts
+++ app/backend/src/controllers/login.controller.ts
@@ -4,10 +4,11 @@ import ICredentials from './interface/Credentials.interface';
 const loginService = new LoginService();
 
 export default class LoginController {
-  // public path: string = '/login';
+  // public path: string = '/login';.
 
   public async create(data: ICredentials): Promise<any> {
     const result = await loginService.create(data)
+
     return result
   }
 }
