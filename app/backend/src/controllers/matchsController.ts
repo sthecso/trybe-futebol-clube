ab8318060207a519ccfa89fef7693e5cd1ed2cb1
Matheus <matheus.pormanhani@gmail.com>
2022-03-19 17:43:11 -0300

    test: Implements GET /matchs?inProgress tests

--- app/backend/src/controllers/matchsController.ts
+++ app/backend/src/controllers/matchsController.ts
@@ -3,6 +3,8 @@ import matchsService from '../services/matchsService';
 
 const matchsController = {
   getAll: async (req: express.Request, res: express.Response) => {
+    const { inProgress } = req.query;
+    console.log(inProgress);
     const matchs = await matchsService.getAll();
     return res.status(200).send(matchs);
   },
--- app/backend/src/tests/matchs.test.ts
+++ app/backend/src/tests/matchs.test.ts
@@ -34,3 +34,55 @@ describe('Tests GET /matchs route', () => {
       });
   })
 });
+describe('Tests GET /matchs?inProgress route', () => {
+  describe('When inProgress is true', () => {
+    it('Return status 200 with an array containing all matches inProgress', async () => {
+      return chai
+        .request(app)
+        .get('/matchs')
+        .query({ inProgress: true })
+        .then((res: Response) => {
+          expect(res.status).to.be.equal(200);
+          expect(res.body).to.be.an('array');
+          res.body.forEach((club: IMatch) => {
+            expect(club).to.have.property('id');
+            expect(club).to.have.property('homeTeam');
+            expect(club).to.have.property('homeTeamGoals');
+            expect(club).to.have.property('awayTeam');
+            expect(club).to.have.property('awayTeamGoals');
+            expect(club).to.have.property('inProgress');
+            expect(club.inProgress).to.be.true;
+            expect(club).to.have.property('homeClub');
+            expect(club.homeClub).to.have.property('clubName');
+            expect(club).to.have.property('awayClub');
+            expect(club.awayClub).to.have.property('clubName');
+          })
+        });
+    })
+  })
+  describe('When inProgress is false', () => {
+    it('Return status 200 with an array containing all matches finalized', async () => {
+      return chai
+        .request(app)
+        .get('/matchs')
+        .query({ inProgress: false })
+        .then((res: Response) => {
+          expect(res.status).to.be.equal(200);
+          expect(res.body).to.be.an('array');
+          res.body.forEach((club: IMatch) => {
+            expect(club).to.have.property('id');
+            expect(club).to.have.property('homeTeam');
+            expect(club).to.have.property('homeTeamGoals');
+            expect(club).to.have.property('awayTeam');
+            expect(club).to.have.property('awayTeamGoals');
+            expect(club).to.have.property('inProgress');
+            expect(club.inProgress).to.be.false;
+            expect(club).to.have.property('homeClub');
+            expect(club.homeClub).to.have.property('clubName');
+            expect(club).to.have.property('awayClub');
+            expect(club.awayClub).to.have.property('clubName');
+          })
+        });
+    })
+  })
+});
