0a21499c5a0791180d6afc6952dae77d3c5d6172
rjanovicci <rjanovicci@hotmail.com>
2022-03-19 16:20:05 -0300

    feat: add bcryptjs

--- app/backend/src/service/login.ts
+++ app/backend/src/service/login.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcryptjs';
 import User from '../database/models/UserModel';
 import throwError from './error';
 
@@ -10,16 +11,15 @@ const verifyLogin = async (loginParams: ILogin) => {
   const { email, password } = loginParams;
   const userFound = await User.findOne({ where: { email } });
   if (!userFound) return throwError('Incorrect email or password', '401');
-  const passFound = userFound?.getDataValue('password');
-  if (password !== passFound) return throwError('Incorrect email or password', '401');
-  if (userFound) {
-    return {
-      id: userFound.id,
-      username: userFound.username,
-      role: userFound.role,
-      email: userFound.email,
-    };
-  }
+  const hashFromBank = userFound.getDataValue('password');
+  const testBCrypt = bcrypt.compareSync(password, hashFromBank);
+  if (!testBCrypt) return throwError('Incorrect email or password', '401');
+  return {
+    id: userFound.id,
+    username: userFound.username,
+    role: userFound.role,
+    email: userFound.email,
+  };
 };
 
 export default verifyLogin;
