18e4c263acb15f90e188d2b227cf451729eecf4e
marcos <marc0sdurval20@gmail.com>
2022-03-19 00:32:04 -0300

    feat: adiciona zod no middleware de error

--- app/backend/src/controller/erros/joi.ts
+++ app/backend/src/controller/erros/joi.ts
@@ -1,15 +1,23 @@
 import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
 enum ListErros{
   'BADREQUEST' = 400,
+  'Unauthorized' = 401,
 }
 class TypesErros {
   private list = ListErros;
 
   public handleErros:ErrorRequestHandler = (err, _req, res, _next) => {
-    const [message, code] = err.split('/');
-    const status = this.list[code];
-    return res.status(+status).json(message);
+    if (err instanceof ZodError) {
+      return res.status(400).json({ message: err.issues[0].message });
+    }
+    const [message, code] = err.message.split('/');
+    if (code) {
+      const status = this.list[code];
+      return res.status(+status).json({ message });
+    }
+    return res.status(500).json('inesperado');
   };
 }
 
