c45e6d68f425f5e6d9e00ca24c9f9e57b63d91b9
Thiago <thiagoc.carboneri@gmail.com>
2022-03-19 09:09:43 -0300

    Refactors ..

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,7 +5,7 @@ class App {
   // ...
 
   constructor() {
-    this.app = express()
+    this.app = express();
     this.config();
   }
 
@@ -16,16 +16,15 @@ class App {
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
-
     this.app.use(accessControl);
-    this.app.use(express.json())
   }
 
   // ...
   public start(PORT: string | number):void {
     this.app.listen(PORT, () => {
       console.log('Running on port', PORT);
-    });  }
+    });
+  }
 }
 
 export { App };
