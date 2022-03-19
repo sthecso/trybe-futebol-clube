f77e9025c032f3a0f97ce7a2890b2b62deb7ef0d
rjanovicci <rjanovicci@hotmail.com>
2022-03-19 00:15:57 -0300

    test: add matchs tests

--- app/backend/src/tests/app.test.ts
+++ app/backend/src/tests/app.test.ts
@@ -79,7 +79,7 @@ describe('All tests', () => {
     });
   })
 
-  describe.only('Test endpoint /clubs', async () => {
+  describe('Test endpoint /clubs', async () => {
     let chaiHttpResponse: Response;
 
     it('status = 200 and returns an array', async () => {
@@ -91,16 +91,64 @@ describe('All tests', () => {
     });
   });
 
-  describe.only('Test endpoint /clubs/:id', async () => {
+  describe('Test endpoint /clubs/:id', async () => {
     let chaiHttpResponse: Response;
 
-    it('Check if it returns what was expected', async () => {
+    it('check if it returns what was expected', async () => {
       chaiHttpResponse = await chai.request(app)
         .get('/clubs/1')
       
       expect(chaiHttpResponse).have.status(200);
       expect(chaiHttpResponse.body).to.be.an('object');
       expect(chaiHttpResponse.body.id).to.be.equal(1);
+    });
+  });
+
+  describe.only('Test endpoint /matchs', async () => {
+    let chaiHttpResponse: Response;
+
+    it('status = 200 and returns an array', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .get('/matchs')
+      
+      expect(chaiHttpResponse).have.status(200);
+      expect(chaiHttpResponse.body).to.be.an('array');
+    });
+
+    it('checks if objects have the correct keys', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .get('/matchs')
+
+      expect(chaiHttpResponse.body[0]).to.haveOwnProperty('id');
+      expect(chaiHttpResponse.body[0]).to.haveOwnProperty('homeTeam');
+      expect(chaiHttpResponse.body[0]).to.haveOwnProperty('homeTeamGoals');
+      expect(chaiHttpResponse.body[0]).to.haveOwnProperty('awayTeam');
+      expect(chaiHttpResponse.body[0]).to.haveOwnProperty('awayTeamGoals');
+      expect(chaiHttpResponse.body[0]).to.haveOwnProperty('inProgress');
+      expect(chaiHttpResponse.body[0]).to.haveOwnProperty('homeClub');
+      expect(chaiHttpResponse.body[0]).to.haveOwnProperty('awayClub');
+    });
+
+    it('check if returns just inProgress = true', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .get('/matchs')
+        .query({ inProgress: 'true' })
+
+      let arr = chaiHttpResponse.body;
+
+      expect(arr[0].inProgress).to.be.equal(true);
+      expect(arr[arr.length-1].inProgress).to.be.equal(true);
+    });
+
+    it('check if returns just inProgress = false', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .get('/matchs')
+        .query({ inProgress: 'false' })
+
+      let arr = chaiHttpResponse.body;
+
+      expect(arr[0].inProgress).to.be.equal(false);
+      expect(arr[arr.length-1].inProgress).to.be.equal(false);
     });
   });
 });
