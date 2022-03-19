e11d3050c092ade1eaa844469d996597c4246b7b
Lanai Conceição <conceicaolanai@gmail.com>
2022-03-19 09:44:08 -0300

    feat: creates login controller

---
+++ app/backend/src/controllers/LoginController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import LoginService from '../services/LoginService';
+
+const LoginController = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const token = await LoginService.login(email, password);
+
+  if (!token) {
+    return res.status(401).json({ error: 'Username or password invalid' });
+  }
+
+  res.status(200).json({ token });
+};
+
+export default LoginController;
