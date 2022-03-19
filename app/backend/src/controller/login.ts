97f1e1912b8fd647af9613a95206472d18f4a81e
marcos <marc0sdurval20@gmail.com>
2022-03-19 00:28:42 -0300

    fix: romove return errorZod do controller login

--- app/backend/src/controller/login.ts
+++ app/backend/src/controller/login.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { ZodError } from 'zod';
 import ValidLogin from './middleware/validLogin';
 import ServiceUser from '../services/user';
 
@@ -9,15 +8,9 @@ class Login {
   private _serviceUser: ServiceUser = new ServiceUser();
 
   public async post(req:Request, res:Response) {
-    try {
-      this._validLogin = new ValidLogin(req.body);
-      const user = await this._serviceUser.getByEmail(req.body);
-      return res.status(200).json({ user });
-    } catch (err) {
-      if (err instanceof ZodError) {
-        return res.status(400).json({ message: err.issues[0].message });
-      }
-    }
+    this._validLogin = new ValidLogin(req.body);
+    const user = await this._serviceUser.getByEmail(req.body);
+    return res.status(200).json({ user });
   }
 }
 
