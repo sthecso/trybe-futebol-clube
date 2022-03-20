58bb0be9fb929315a3285612eeeac6f5b4cf48dc
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-03-19 23:17:50 -0300

    feat: criando a logica que valida se o email existe no banco

--- app/backend/src/controller/Login.ts
+++ app/backend/src/controller/Login.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import Token from '../auth/Token';
 import LoginService from '../service/Login';
 
@@ -20,6 +21,11 @@ class Login {
     this.router.post('/', async (req, res) => {
       const { email, password } = req.body;
       this.loginService = await new LoginService(email, password).findUser();
+
+      if (!this.loginService) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          message: 'Incorrect email or password' });
+      }
 
       const token = await Token.generate({ email });
       return res.status(200).json({
--- app/backend/src/service/Login.ts
+++ app/backend/src/service/Login.ts
@@ -30,4 +30,3 @@ class Login {
 }
 
 export default Login;
-
