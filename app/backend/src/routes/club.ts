df747cbf3391c029df7b08f3363d1f5814dd0351
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-20 00:08:44 -0300

    feat: implement remaining routers

---
+++ app/backend/src/routes/club.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const clubRouter = Router();
+
+export default clubRouter;
---
+++ app/backend/src/routes/match.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const matchRouter = Router();
+
+export default matchRouter;
