1b2884a3b167afe4daf77f2e8e35f3469a97d82d
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-19 23:31:22 -0300

    refactor: import and redirect models from index.ts

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,5 +1,14 @@
 import { Sequelize } from 'sequelize';
+import User from './User';
+import Club from './Club';
+import Match from './Match';
 
 const databaseConfig = require('../config/database');
+
+export {
+  User,
+  Club,
+  Match,
+};
 
 export default new Sequelize(databaseConfig);
