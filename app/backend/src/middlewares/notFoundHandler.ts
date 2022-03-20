a948f73301167ab1e0aa7cef7e332dd818366ff6
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-19 23:32:35 -0300

    refactor: use RequestHandler to type the input data

--- app/backend/src/middlewares/notFoundHandler.ts
+++ app/backend/src/middlewares/notFoundHandler.ts
@@ -1,11 +1,11 @@
-import { Request, Response, NextFunction } from 'express';
+import { RequestHandler } from 'express';
 
-const notFoundHandler = (req: Request, res: Response, _next: NextFunction) => {
+const notFoundHandler: RequestHandler = (req, res, _next) => {
   const entityName = req.originalUrl.slice(1);
   const entity = entityName.slice(0, 1).toUpperCase()
     .concat(entityName.slice(1).toLowerCase());
-  const message = `${entity} not found`;
-  return res.status(404).send({ error: message });
+  const error = `${entity} not found`;
+  return res.status(404).send({ error });
 };
 
 export default notFoundHandler;
