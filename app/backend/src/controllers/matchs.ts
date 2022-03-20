b15bc35aa0ab02f19658e67454f2faa07d1d6c34
rjanovicci <rjanovicci@hotmail.com>
2022-03-20 02:04:29 -0300

    feat: add post & patch /matchs

--- app/backend/src/controllers/matchs.ts
+++ app/backend/src/controllers/matchs.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import { Request, Response, NextFunction } from 'express';
-import findAllMatchs from '../service/matchs';
+import matchsValidation from '../middlewares/validation/matchsValidation';
+import { findAllMatchs, createMatch, updateInProgress } from '../service/matchs';
 
 const routerMatchs = express.Router();
 
@@ -10,6 +11,32 @@ routerMatchs.get(
     try {
       const result = await findAllMatchs(req.query.inProgress as string);
       return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+routerMatchs.post(
+  '/',
+  matchsValidation,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await createMatch(req.body);
+      return res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+routerMatchs.patch(
+  '/:id/finish',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const updated = await updateInProgress(Number(id));
+      return res.status(200).json(updated);
     } catch (error) {
       next(error);
     }
