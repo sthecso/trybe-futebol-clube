49c0e629bad9dafd57de27e91d69e15d886aa85c
pedroalles <pedroalles@gmail.com>
2022-03-19 22:06:00 -0300

    feat(req-03): implement user model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,50 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '.';
+
+class User extends Model {
+  public id: number;
+
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'User',
+    tableName: 'users',
+  },
+);
+
+export default User;
--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -6,3 +6,4 @@ export default new Sequelize(databaseConfig);
 
 export { default as ClubModel } from './Club';
 export { default as MatchModel } from './Match';
+export { default as UserModel } from './User';
