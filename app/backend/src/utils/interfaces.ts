187d2b3cab179969c596ee7f89a8f6c984b8b9f9
gesse_carlos <gesse.dev@outlook.com>
2022-03-20 07:07:04 -0300

    create interfaces for login

--- app/backend/src/utils/interfaces.ts
+++ app/backend/src/utils/interfaces.ts
@@ -4,3 +4,52 @@ export interface IPayload {
   email: string;
   role: string;
 }
+
+export interface ICredential {
+  email: string;
+  password: string;
+}
+
+export interface IToken {
+  id: number;
+  username: string;
+  email: string;
+  role: string;
+}
+
+export interface IUser {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+export interface IUserRepository {
+  getByEmail(email: string): Promise<IUser | null>
+}
+
+export interface ILoginResponse {
+  code: number;
+  data:
+  {
+    message: string;
+    user?: undefined;
+    token?: undefined;
+  }
+  |
+  {
+    user: {
+      id: number;
+      email: string;
+      role: string;
+      username: string;
+    };
+    token: string;
+    message?: undefined;
+  };
+}
+
+export interface ILoginService {
+  login(credentials: ICredential): Promise<ILoginResponse>
+}
