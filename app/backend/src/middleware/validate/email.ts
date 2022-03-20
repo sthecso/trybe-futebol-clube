e821645c7fd1bcd1f5a1015cd063fe1a60100f7f
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-03-20 01:36:54 -0300

    feat(middleware): função que valida o email

--- app/backend/src/middleware/validate/email.ts
+++ app/backend/src/middleware/validate/email.ts
@@ -1,4 +1,12 @@
-// import { NextFunction, Request } from "express";
-// function validateEmail (req:Request, res: Response, next: NextFunction) {
-// }
-// export default validateEmail
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+function validateEmail(req:Request, res: Response, next: NextFunction) {
+  const { email = null } = req.body;
+  if (!email) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: 'All fields must be filled' });
+  }
+  next();
+}
+export default validateEmail;
