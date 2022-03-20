0c0639e215846dcf267fb32d16ad8116e96a11af
rjanovicci <rjanovicci@hotmail.com>
2022-03-20 01:53:10 -0300

    feat: add matchs service functions

--- app/backend/src/service/matchs.ts
+++ app/backend/src/service/matchs.ts
@@ -1,5 +1,14 @@
 import Clubs from '../database/models/ClubModel';
 import Matchs from '../database/models/MatchModel';
+import throwError from './error';
+
+interface IMatchDate {
+  homeTeam: number,
+  homeTeamGoals: number,
+  awayTeam: number,
+  awayTeamGoals: number,
+  inProgress: boolean
+}
 
 const findAllMatchs = async (param: string) => {
   if (param === 'true' || param === 'false') {
@@ -22,4 +31,58 @@ const findAllMatchs = async (param: string) => {
   return allMatchs;
 };
 
-export default findAllMatchs;
+const createMatch = async (matchDate: object) => {
+  const { homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress } = matchDate as IMatchDate;
+  const createdMatch = await Matchs.create({
+    homeTeam,
+    homeTeamGoals,
+    awayTeam,
+    awayTeamGoals,
+    inProgress,
+  });
+
+  return createdMatch;
+};
+
+interface IFoundMatch {
+  id: number,
+  homeTeam: number,
+  homeTeamGoals: number,
+  awayTeam: number,
+  awayTeamGoals: number,
+  inProgress: boolean,
+}
+
+const findMatchById = async (id: number) => {
+  const foundMatch = await Matchs.findOne({ where: { id } }) as IFoundMatch;
+  if (!foundMatch) return throwError('Match not found', '404');
+
+  return {
+    id: foundMatch.id,
+    homeTeam: foundMatch.homeTeam,
+    homeTeamGoals: foundMatch.homeTeamGoals,
+    awayTeam: foundMatch.awayTeam,
+    awayTeamGoals: foundMatch.awayTeamGoals,
+    inProgress: foundMatch.inProgress,
+  };
+};
+
+const updateInProgress = async (id: number) => {
+  await Matchs.update({ inProgress: false }, { where: { id } });
+  const updated = await findMatchById(id);
+
+  return updated;
+};
+
+const findClubById = async (id: number) => {
+  const result = await Clubs.findOne({ where: { id } });
+
+  return result;
+};
+
+export {
+  findAllMatchs,
+  createMatch,
+  updateInProgress,
+  findClubById,
+};
