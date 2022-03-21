cfab05ab323f7a7dd925914aa206491b24e1b7ff
Felipe Lima <felipe_lima10@icloud.com>
2022-03-20 22:35:27 -0300

    fix: requirement 4 and 5

--- app/backend/src/schemas/loginSchema.ts
+++ app/backend/src/schemas/loginSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const loginSchema = z.object({
-  email: z.string({ required_error: 'Username is required' }),
+  email: z.string({ required_error: 'email is required' }).email(),
   password: z.string({ required_error: 'Password is required' }),
 });
 
