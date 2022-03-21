cd3244c98e4686a0037e3e76ae44f122f380a013
Breno Lopes <brenokos@gmail.com>
2022-03-21 10:24:42 -0300

    fix: joi fixed

--- app/backend/src/middlewares/joi/login.ts
+++ app/backend/src/middlewares/joi/login.ts
@@ -6,17 +6,17 @@ const message = 'All fields must be filled'; // temporário
 const schema = Joi.object({
   email: Joi.string().empty().required().messages({
     'any.required': message,
-    'any.empty': message,
+    'string.empty': message,
   }),
   password: Joi.string().empty().required().messages({
     'any.required': message,
-    'any.empty': message,
+    'string.empty': message,
   }),
 });
 
 export default async (req: Request, res: Response, next: NextFunction) => {
   const { error } = schema.validate(req.body);
-
+  console.log(error?.message);
   if (error) {
     if (error.message.includes('filled')) return res.status(401).json({ message: error.message });
     return res.status(422).json({ message: error.message });
