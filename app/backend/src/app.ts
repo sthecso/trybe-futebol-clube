11bf192f2287a6050367e5b385685539cc8751c7
Murilo Rainho <murilorainho01@gmail.com>
2022-03-21 19:53:09 -0300

    [feat]: create (finally) an error handler

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,11 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+
 import * as express from 'express';
 
 import * as bodyParse from 'body-parser';
 
 import { Login, Club, Match, Leaderboards } from './routers';
 
+import { HttpStatusCode } from './utils';
+
 class App {
   public app: express.Express;
+
+  private httpStatusCode = HttpStatusCode;
 
   private loginRouter = new Login();
 
@@ -31,15 +37,28 @@ class App {
     this.app.use('/matchs', this.matchRouter.router);
 
     this.app.use('/leaderboard', this.leaderboardsRouter.router);
+
+    this.app.use((error: unknown, _req: Request, res: Response, _next: NextFunction) => {
+      if (error instanceof Error) {
+        return res
+          .status(this.httpStatusCode.InternalServerError)
+          .json({ message: error.message });
+      }
+
+      return res
+        .status(this.httpStatusCode.InternalServerError)
+        .json({ message: 'Something is wrong' });
+    });
   }
 
   private config():void {
-    const accessControl: express.RequestHandler = (_req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-      res.header('Access-Control-Allow-Headers', '*');
-      next();
-    };
+    const accessControl: express
+      .RequestHandler = (_req: Request, res: Response, next: NextFunction) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+        res.header('Access-Control-Allow-Headers', '*');
+        next();
+      };
 
     this.app.use(accessControl);
   }
