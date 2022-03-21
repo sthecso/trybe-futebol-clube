b0c6c6ec0e3bbe177e34ded8c6f66fd24b7eab01
Lanai Conceição <conceicaolanai@gmail.com>
2022-03-21 18:48:36 -0300

    refactor: switches position of if

--- app/backend/src/controllers/middlewares/validateEmail.ts
+++ app/backend/src/controllers/middlewares/validateEmail.ts
@@ -3,11 +3,11 @@ import { NextFunction, Request, Response } from 'express';
 const validateEmail = (req: Request, res: Response, next:NextFunction) => {
   const { email } = req.body;
 
-  if (email === '') {
-    return res.status(400).json({ message: '"email" is not allowed to be empty' });
-  }
+  if (!email) return res.status(401).json({ message: 'All fields must be filled' });
 
-  if (!email) return res.status(400).json({ message: '"email" is required' });
+  if (email === '') {
+    return res.status(401).json({ message: 'Incorrect email or password' });
+  }
 
   next();
 };
