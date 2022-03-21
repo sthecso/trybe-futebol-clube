0b29cf79d07efe4bb566faf9f6ea9c5e291c5854
Fernando Costa <fgosuendacosta@gmail.com>
2022-03-21 14:31:32 -0300

    refactor(backend): adjustments in messages

--- app/backend/src/database/controllers/Matchs/postMatchController.ts
+++ app/backend/src/database/controllers/Matchs/postMatchController.ts
@@ -3,12 +3,20 @@ import { UserSentMatchData } from '../../../helpers/Interfaces';
 import getClubByIdService from '../../services/Clubs/getClubByIdService';
 import postMatchService from '../../services/Matchs/postMatchService';
 
+const MSG_INSERT_VALID_GOALS = { message: 'Insira um número de gols válido' };
+const MSG_INSERT_GAME_PROGRESS = { message: 'Insira se a partida está em progresso ou não' };
+const MSG_CREATE_MATCH_TEAMS = {
+  message: 'It is not possible to create a match with two equal teams',
+};
+const MSG_INSERT_VALID_TEAM = { message: 'Insira um time válido' };
+const MSG_ERROR_TEAM_ID = { message: 'There is no team with such id!' };
+
 function verifyData(matchData: UserSentMatchData) {
   if (matchData.awayTeamGoals === undefined || matchData.homeTeamGoals === undefined) {
-    return { code: 401, payload: { message: 'Insira um número de gols válido' } };
+    return { code: 401, payload: MSG_INSERT_VALID_GOALS };
   }
   if (matchData.inProgress === undefined) {
-    return { code: 401, payload: { message: 'Insira se a partida está em progresso ou não' } };
+    return { code: 401, payload: MSG_INSERT_GAME_PROGRESS };
   }
   return { code: 200 };
 }
@@ -17,15 +25,15 @@ async function verifyTeams(team1: number | string, team2: number | string) {
   if (team1 === team2) {
     return {
       code: 401,
-      payload: { message: 'It is not possible to create a match with two equal teams' } };
+      payload: MSG_CREATE_MATCH_TEAMS };
   }
   if (team1 === undefined || team2 === undefined) {
-    return { code: 401, payload: { message: 'Insira um time válido' } };
+    return { code: 401, payload: MSG_INSERT_VALID_TEAM };
   }
   const club1 = await getClubByIdService(team1);
   const club2 = await getClubByIdService(team2);
   if (!club1 || !club2) {
-    return { code: 401, payload: { message: 'There is no team with such id!' } };
+    return { code: 401, payload: MSG_ERROR_TEAM_ID };
   }
   return { code: 200 };
 }
