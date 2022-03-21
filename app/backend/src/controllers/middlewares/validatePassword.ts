6772a70ad8d5b349e35bee5fab9577272b411dd6
Lanai Conceição <conceicaolanai@gmail.com>
2022-03-21 18:50:49 -0300

    refactor: switches position of if

--- app/backend/src/controllers/middlewares/validatePassword.ts
+++ app/backend/src/controllers/middlewares/validatePassword.ts
@@ -3,11 +3,11 @@ import { NextFunction, Request, Response } from 'express';
 const validatePassword = (req: Request, res: Response, next:NextFunction) => {
   const { password } = req.body;
 
-  if (password === '') {
-    return res.status(400).json({ message: '"password" is not allowed to be empty' });
-  }
+  if (!password) return res.status(401).json({ message: 'All fields must be filled' });
 
-  if (!password) return res.status(400).json({ message: '"password" is required' });
+  if (password === '') {
+    return res.status(401).json({ message: 'Incorrect email or password' });
+  }
 
   next();
 };
