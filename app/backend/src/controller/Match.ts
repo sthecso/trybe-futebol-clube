f94a7dad0bca73428baf8745bddb0bd89ee2d99a
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-03-21 17:45:39 -0300

    feat(Controller): criando a logica do Match

---
+++ app/backend/src/controller/Match.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import MatchService from '../service/Match';
+
+class Match {
+  public router = Router();
+
+  constructor() {
+    this.get();
+  }
+
+  get() {
+    this.router.get('/', async (req: Request, res: Response) => {
+      const { inProgress } = req.query;
+      if (typeof inProgress === 'string') {
+        const matchs = await MatchService.findOneByInProgress(inProgress);
+        return res.status(200).json(matchs);
+      }
+      const matchs = await MatchService.findAll();
+      return res.status(200).json(matchs);
+    });
+  }
+}
+
+export default new Match().router;
