1b8a18fa4945f1711d730ad463682acfdcfe0257
Fernando Costa <fgosuendacosta@gmail.com>
2022-03-21 14:27:59 -0300

    refactor(backend): refactoring message controllers

--- app/backend/src/database/controllers/Login/LoginController.ts
+++ app/backend/src/database/controllers/Login/LoginController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import loginService from '../../services/LoginService';
 
+const MSG_FIELDS_FILLED = { message: 'All fields must be filled' };
+
 async function loginController(req: Request, res: Response) {
   const { email, password } = req.body;
   if (email === undefined || email === '' || password === undefined || password === '') {
-    return res.status(401).json({ message: 'All fields must be filled' });
+    return res.status(401).json(MSG_FIELDS_FILLED);
   }
   const response = await loginService(email, password);
   return res.status(response.code).json(response.payload);
--- app/backend/src/database/controllers/Matchs/finishMatchController.ts
+++ app/backend/src/database/controllers/Matchs/finishMatchController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import finishMatchService from '../../services/Matchs/finishMatchService';
 
+const MSG_GAME_FINALLIZED = { message: 'Partida finalizada!' };
+
 async function finishMatchController(req: Request, res: Response) {
   const { id } = req.params;
   await finishMatchService(id);
 
-  return res.status(200).json({ message: 'Partida finalizada!' });
+  return res.status(200).json(MSG_GAME_FINALLIZED);
 }
 
 export default finishMatchController;
