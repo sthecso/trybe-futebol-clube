d675f1b171f4b2e94cbd9474f14785dbfe0fb022
Filipe <filipecandido123@hotmail.com>
2022-03-22 03:37:07 -0300

    refactor: criado um arquivo separado de criaçao de token para ser introduzido no login/services e manter o codigo limpo

---
+++ app/backend/src/auth/createTokenJWT.ts
@@ -0,0 +1,13 @@
+import * as jwt from 'jsonwebtoken';
+import jwtConfig from '../utils';
+
+export default class Token {
+  static createToken(searchUser: string) {
+    const dataEmail = searchUser;
+    const secret = jwtConfig.jwt.secret as string;
+
+    const token = jwt.sign({}, secret, { subject: dataEmail, expiresIn: '7d', algorithm: 'HS256' });
+
+    return token;
+  }
+}
