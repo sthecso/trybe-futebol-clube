1d00a54a09f09a70279e021ee9e779598ee125dc
Filipe <filipecandido123@hotmail.com>
2022-03-22 02:32:02 -0300

    refactor: /validate.login retirado regex

--- app/backend/src/middlewares/validate.login.ts
+++ app/backend/src/middlewares/validate.login.ts
@@ -1,20 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 
 const MESSAGE_FIELD = 'All fields must be filled';
-// tentativa pra fazer funcionar o meu teste de integração
-const regexEmail = (email: string) => {
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email);
-};
 
 const validateEmail = async (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
 
   if (!email || email === undefined) {
-    return res.status(401)
-      .json({ message: MESSAGE_FIELD });
-  }
-  if (!regexEmail(email)) {
     return res.status(401)
       .json({ message: MESSAGE_FIELD });
   }
