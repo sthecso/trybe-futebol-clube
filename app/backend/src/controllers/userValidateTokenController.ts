737ce0450c659f9a5549f6f9f6f117473f5b194f
samuel <samuel.guilherme.l@hotmail.com>
2022-03-22 12:02:16 -0300

    fix: validateAuth

--- app/backend/src/controllers/userValidateTokenController.ts
+++ app/backend/src/controllers/userValidateTokenController.ts
@@ -1,11 +1,8 @@
 import { Request, Response } from 'express';
-import JwtMethods from '../utils/jwtMethods';
 import StatusCode from '../utils/statusCode';
 
 class UserValidateTokenController {
   private statusCode = StatusCode;
-
-  private jwtUtils = new JwtMethods();
 
   constructor() {
     this.verifyControllerAuth = this.verifyControllerAuth.bind(this);
--- app/backend/src/middlewares/validateAuth.ts
+++ app/backend/src/middlewares/validateAuth.ts
@@ -15,24 +15,23 @@ class ValidateAuth {
   async verifyToken(req: Request, res:Response, next: NextFunction) {
     const { authorization } = req.headers as unknown as IAuth;
     const token = authorization.split(' ')[1];
-
+    const message = 'usuario nao authenticado';
     if (!authorization || !authorization.length) {
       return res.status(this.StatusCode.Unauthorized)
-        .json({ message: 'usuario nao authenticado' });
+        .json({ message });
     }
 
     try {
       const dataDecoded = this.jwtUtils.verifyToken(token);
       if (dataDecoded === null) {
-        return res.status(this.StatusCode.Unauthorized)
-          .json({ message: 'usuario nao authenticado' });
+        return res.status(this.StatusCode.Unauthorized).json({ message });
       }
 
       req.decodedUser = dataDecoded;
 
       next();
     } catch (err) {
-      console.log(`usuario nao authencitado${err}`);
+      return res.status(this.StatusCode.Unauthorized).json({ message });
     }
   }
 }
