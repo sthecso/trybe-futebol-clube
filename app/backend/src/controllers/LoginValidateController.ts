aaa8a67789b76e46827821aee5f80f9a4642a19c
Lanai Conceição <conceicaolanai@gmail.com>
2022-03-22 14:41:30 -0300

    feat: creates loginValidate controller

---
+++ app/backend/src/controllers/LoginValidateController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import LoginValidateService from '../services/LoginValidateService';
+
+const LoginController = async (req: Request, res: Response) => {
+  const { id } = req.body;
+  const idNumber = Number(id);
+  const role = await LoginValidateService.login(idNumber);
+
+  res.status(200).json(role);
+};
+
+export default LoginController;
