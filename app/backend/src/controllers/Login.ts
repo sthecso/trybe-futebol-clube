c0df7f258d0fa78c3208b096801d4d0cdf73f525
Filipe <filipecandido123@hotmail.com>
2022-03-22 03:37:31 -0300

    refactor: retirado metodo arrow function para metodo static e code clean

--- app/backend/src/controllers/Login.ts
+++ app/backend/src/controllers/Login.ts
@@ -1,36 +1,22 @@
 import { Request, Response } from 'express';
-import * as jwt from 'jsonwebtoken';
 import { LoginService } from '../services';
-import jwtConfig from '../utils';
 
 class LoginController {
-  getLogin = async (req: Request, res: Response) => {
+  static async getLogin(req: Request, res: Response) {
     const { email, password } = req.body;
-    const { code, data } = await LoginService.getLogin({ email, password });
-    if (data.message) {
-      console.log(data.message);
-      return res.status(code).json(data.message);
+    const login = await LoginService.getLogin({ email, password });
+
+    if (login.message) {
+      return res.status(login.code).json({ message: login.message });
     }
 
-    const dataEmail = data.email;
-    const secret = jwtConfig.jwt.secret as unknown as string;
+    res.status(200).json(login);
+  }
 
-    const token = jwt.sign({}, secret, {
-      subject: dataEmail,
-      expiresIn: '7d',
-      algorithm: 'HS256',
-    });
-    const { id, username, role } = data;
-    res.status(code).json({ user: {
-      id, username, role, email,
-    },
-    token });
-  };
-
-  getUser = async (_req: Request, res: Response) => {
+  static async getUser(_req: Request, res: Response) {
     const { code, data } = await LoginService.getUser();
     res.status(code).json(data);
-  };
+  }
 }
 
-export default new LoginController();
+export default LoginController;
