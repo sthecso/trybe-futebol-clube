f379f420ebb143c0b6b5ab8b5075a4279375452f
Filipe <filipecandido123@hotmail.com>
2022-03-21 23:51:02 -0300

    feat: adicionado arquivos clubs em services e controllers

---
+++ app/backend/src/controllers/Clubs.ts
@@ -0,0 +1,10 @@
+// import { Request, Response } from 'express';
+// import LoginService from '../services';
+
+// class ClubsController {
+//   getClubs = async (req: Request, res: Response) => {
+//     const clubs = await ClubService;
+//   };
+// }
+
+// export default new ClubsController();
