0bb32638b61349764d99d612d0752d880a5cce8c
Fernando Costa <fgosuendacosta@gmail.com>
2022-03-22 11:02:27 -0300

    test: creating new tests for Clubs

--- app/backend/src/tests/Clubs.spec.ts
+++ app/backend/src/tests/Clubs.spec.ts
@@ -7,6 +7,25 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const dataMockTeams = [
+  { id: 1, clubName: 'Avaí/Kindermann' },
+  { id: 2, clubName: 'Bahia' },
+  { id: 3, clubName: 'Botafogo' },
+  { id: 4, clubName: 'Corinthians' },
+  { id: 5, clubName: 'Cruzeiro' },
+  { id: 6, clubName: 'Ferroviária' },
+  { id: 7, clubName: 'Flamengo' },
+  { id: 8, clubName: 'Grêmio' },
+  { id: 9, clubName: 'Internacional' },
+  { id: 10, clubName: 'Minas Brasília' },
+  { id: 11, clubName: 'Napoli-SC' },
+  { id: 12, clubName: 'Palmeiras' },
+  { id: 13, clubName: 'Real Brasília' },
+  { id: 14, clubName: 'Santos' },
+  { id: 15, clubName: 'São José-SP' },
+  { id: 16, clubName: 'São Paulo' },
+];
+
 const ROUTE_CLUBS = '/clubs';
 const ROUTE_CLUBS_ID = '/clubs/1';
 
@@ -18,6 +37,12 @@ describe('Clubs Test', () => {
       chaiHttpResponse = await chai.request(app).get(ROUTE_CLUBS)
       expect(chaiHttpResponse.status).to.be.equal(200);
     });
+    it('Return check data teams', async () => {
+      const response: Response = await chai
+        .request(app).get(ROUTE_CLUBS).set('content-type', 'application/json');
+      expect((response.body)).to.deep.equals(dataMockTeams);
+      expect(response.status).to.equals(200);
+    });
   });
 
   describe('When Request with id', () => {
@@ -25,6 +50,13 @@ describe('Clubs Test', () => {
     it('Return status code 200', async () => {
       chaiHttpResponse = await chai.request(app).get(ROUTE_CLUBS_ID)
       expect(chaiHttpResponse.status).to.be.equal(200);
+    });
+    it('Return team with specified id', async () => {
+      const id = 4;
+      const response: Response = await chai
+        .request(app).get(`${ROUTE_CLUBS}/${id}`).set('content-type', 'application/json');
+      expect((response.body)).to.deep.equals({ id: 4, clubName: 'Corinthians' });
+      expect(response.status).to.equals(200);
     });
   });
 });
