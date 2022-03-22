a8599bf70778d3816ca76c7d043cd4892b95dfdc
Filipe <filipecandido123@hotmail.com>
2022-03-21 23:40:57 -0300

    feat: substituido crud padrao para crudPersonalizado, adicionado bcryptjs para a criaçao de hash no password e redirecionando novos dados quando o usuario fazer login, ao fazer login retorna, id, username, role pertecente aquele email logado

--- app/backend/src/services/Login.ts
+++ app/backend/src/services/Login.ts
@@ -1,8 +1,9 @@
-import CRUD from '../interfaces/CRUD';
+import { CRUDLogin } from '../interfaces/CRUD';
 import { IEmailAndPasswordDTO, LoginDTO } from '../interfaces/ILogin';
 import User from '../database/models/User';
+import { hash, compare } from '../utils/Bcrypt';
 
-class LoginService implements CRUD {
+class LoginService implements CRUDLogin {
   userReturnOnlogin = async (email: string) => {
     const attributesUser = await User.findAll() as LoginDTO[];
     const filteringEmailUser = attributesUser.filter((e) => e.email === email);
@@ -17,25 +18,37 @@ class LoginService implements CRUD {
     return recoverAttributesUserLogged;
   };
 
+  checkIfPasswordTrueOrFalse = async (password: string, user: string) => {
+    const hashPassword = await hash(password);
+
+    const comparePassword = await compare(hashPassword, user);
+
+    return comparePassword;
+  };
+
   getLogin = async (value: IEmailAndPasswordDTO) => {
     const { email, password } = value;
-    const searchUser = await User.findOne({ where: { email, password } }) as User;
+    const searchUser = await User.findOne({ where: { email } }) as User;
 
-    if (!searchUser) {
+    if (!searchUser || searchUser === undefined) {
       return { code: 401, data: { message: 'Incorrect email or password' } };
     }
-    if (password !== searchUser.password) {
+
+    const comparePassword = await this.checkIfPasswordTrueOrFalse(password, searchUser.password);
+
+    if (comparePassword) {
       return { code: 401, data: { message: 'Incorrect email or password' } };
     }
+
     const userLogged = await this.userReturnOnlogin(email);
 
     return { code: 200,
       data: {
-        id: String(userLogged[0].id),
+        id: userLogged[0].id,
         username: userLogged[0].username,
         role: userLogged[0].role,
         email: searchUser.email,
-        password: searchUser.password,
+        password,
       } };
   };
 
