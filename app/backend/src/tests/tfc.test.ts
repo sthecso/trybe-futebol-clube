4b4ba2e2e2a86a8762e6a175cd11029303132796
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-23 14:25:48 -0300

    test: comment previous test implementations

--- app/backend/src/tests/tfc.test.ts
+++ app/backend/src/tests/tfc.test.ts
@@ -1,9 +1,10 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import { Model } from 'sequelize';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import User from '../database/models/User';
+import { User } from '../database/models';
 
 import { Response } from 'superagent';
 
@@ -12,10 +13,24 @@ chai.use(chaiHttp);
 const { expect } = chai;
 const tokenExpression = /^ey[\w-]+\.ey[\w-]+\.[\w-]+$/
 
-describe('Verifica se o usuário fez login', () => {
-  it('Procura um usuário no banco de dados', async () => {
-    const user = await User.findOne();
-    console.log(typeof user);
-    expect(user).to.be.a('string');
-  });
-});
+// describe('Verifica se o usuário fez login', () => {
+//   /*
+//   {
+//     id: 2,
+//     username: 'User',
+//     role: 'user',
+//     email: 'user@user.com',
+//     password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
+//   }
+//   */
+//   // password: secret_user
+//   it('Procura um usuário no banco de dados', async () => {
+//     const { dataValues: user } = await User.findOne({ where: { id: 2 } }) as any;
+//     expect(user).to.be.an('object');
+//     expect(user).to.have.property('id');
+//     expect(user).to.have.property('username');
+//     expect(user).to.have.property('role');
+//     expect(user).to.have.property('email');
+//     expect(user).to.have.property('password');
+//   });
+// });
