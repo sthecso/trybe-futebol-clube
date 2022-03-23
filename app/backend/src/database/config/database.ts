4fc1e41ff490ca7019cbd4a9a4d79ebc61f45852
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-23 14:29:33 -0300

    refator: change syntax to fit typescript

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,14 +1,24 @@
-require('dotenv').config();
+import 'dotenv/config';
 
-module.exports = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || '3002',
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
+const username = process.env.DB_USER || 'root';
+const password = process.env.DB_PASS || '123456';
+const database = process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE';
+const host = process.env.DB_HOST || '127.0.0.1';
+const port = process.env.DB_PORT || '3002';
+
+const dialect = 'mysql';
+const dialectOptions = {
+  timezone: 'Z',
+};
+const logging = false;
+
+export {
+  username,
+  password,
+  database,
+  host,
+  port,
+  dialect,
+  dialectOptions,
+  logging,
 };
