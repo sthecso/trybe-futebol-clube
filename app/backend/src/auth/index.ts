510ae8e0d250de369fca575f97d4deb03acb021e
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-23 14:43:25 -0300

    refactor: export methods from auth directory

--- app/backend/src/auth/index.ts
+++ app/backend/src/auth/index.ts
@@ -1,51 +1,11 @@
-import * as fs from 'fs';
-import { resolve } from 'path';
-import { Response, NextFunction } from 'express';
-import { sign, decode, verify, JwtPayload, SignOptions } from 'jsonwebtoken';
-import { User } from '../database/models';
-import { IRequest } from '../utils/interfaces';
-import { Authorization } from '../utils/types';
-import HttpException from '../classes/httpException';
+import { createToken, readToken, validateToken } from './jwt';
+import { createSalt, createHash, validatePassword } from './bcryptjs';
 
-const jwtFile = resolve(__dirname, '../..', 'jwt.evaluation.key');
-const JWT_SECRET = fs.readFileSync(jwtFile, 'utf-8');
-const jwtConfig: SignOptions = {
-  expiresIn: '30d',
-  algorithm: 'HS256',
+export {
+  createToken,
+  readToken,
+  validateToken,
+  createSalt,
+  createHash,
+  validatePassword,
 };
-
-const verifyTokenPresence = (authorization: Authorization): void => {
-  if (!authorization) {
-    const error = new HttpException(401, 'Token not found');
-    throw error;
-  }
-};
-
-const verifyTokenValidity = (token: string): void => {
-  try {
-    verify(token, JWT_SECRET);
-  } catch (err) {
-    const error = new HttpException(401, 'Expired or invalid token');
-    throw error;
-  }
-};
-
-export function createToken(inputData: User): string {
-  const { id, username, role, email } = inputData;
-  const userData = { id, username, role, email };
-  return sign(userData, JWT_SECRET, jwtConfig);
-}
-
-export function readToken(authorization: Authorization): JwtPayload {
-  const token = authorization || '';
-  return decode(token) as JwtPayload;
-}
-
-export function validateToken(req: IRequest, _res: Response, next: NextFunction): void {
-  const { authorization } = req.headers;
-  const token = authorization || '';
-  verifyTokenPresence(authorization);
-  verifyTokenValidity(token);
-  req.user = decode(token) as JwtPayload;
-  next();
-}
