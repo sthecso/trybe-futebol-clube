1c89128474130252ebaaeec6dc65b43776ee5e2f
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-23 14:33:32 -0300

    refactor: export sequelize instance and constructor

--- app/backend/src/database/models/index.ts
+++ app/backend/src/database/models/index.ts
@@ -1,14 +1,16 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Options } from 'sequelize';
+import * as databaseOptions from '../config/database';
 import User from './User';
 import Club from './Club';
 import Match from './Match';
 
-const databaseConfig = require('../config/database');
+const databaseConfig = databaseOptions as unknown as Options;
+const sequelize = new Sequelize(databaseConfig);
 
 export {
+  Sequelize,
+  sequelize,
   User,
   Club,
   Match,
 };
-
-export default new Sequelize(databaseConfig);
