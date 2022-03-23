637c18805a5ff4b0215073544c55a2b6e8d01236
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-03-23 18:24:06 -0300

    feat(service): criando interfaces

--- app/backend/src/service/Match.ts
+++ app/backend/src/service/Match.ts
@@ -1,10 +1,14 @@
-import { Op } from 'sequelize';
 import Match from '../database/models/matchs';
 import Club from '../database/models/club';
 
-type UpdatePatch = {
+interface UpdatePatch {
   id: number;
-};
+}
+
+interface UpdatePatchTeam extends UpdatePatch {
+  homeTeamGoals: number;
+  awayTeamGoals: number;
+}
 
 class MatchService {
   static async findAll() {
@@ -16,7 +20,6 @@ class MatchService {
     return allMatch;
   }
 
- 
   static async findOneByInProgress(inProgress: string) {
     const oneMatch = await Match.findAll({ where: { in_progress: JSON.parse(inProgress) },
       include: [
@@ -36,6 +39,17 @@ class MatchService {
 
     const result = await Match.update({
       inProgress: false,
+    }, { where: { id } });
+
+    return result[0];
+  }
+
+  static async updatePatchTeamGoals(params: UpdatePatchTeam) {
+    const { id, homeTeamGoals, awayTeamGoals } = params;
+
+    const result = await Match.update({
+      homeTeamGoals,
+      awayTeamGoals,
     }, { where: { id } });
 
     return result[0];
