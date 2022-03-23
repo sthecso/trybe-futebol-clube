946471383a06f38948359bcda2bd622372a673e1
Filipe <filipecandido123@hotmail.com>
2022-03-23 19:26:55 -0300

    feat: adicionado no service/clubs metodo que recupera um array de objetos de clubs

--- app/backend/src/services/Clubs.ts
+++ app/backend/src/services/Clubs.ts
@@ -1,11 +1,10 @@
-import { CRUDClub } from '../interfaces/CRUD';
 import Clubs from '../database/models/Club';
 
-class ClubService implements CRUDClub {
-  getClubs = async () => {
+class ClubService {
+  static async getClubs() {
     const clubs = await Clubs.findAll();
     return { code: 200, data: clubs };
-  };
+  }
 }
 
-export default new ClubService();
+export default ClubService;
