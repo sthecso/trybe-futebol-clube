2ce2fe3867eae80cc8e6cfe2e3a3509f6129bed8
Elaine <elainefelipemoreira@gmail.com>
2022-03-23 15:07:15 -0300

    Altera erro de portugues no comentario

--- app/backend/src/controller/ClubController.ts
+++ app/backend/src/controller/ClubController.ts
@@ -1,7 +1,7 @@
 import GetAll from '../services/clubService';
 
 export default class GetAllClubs {
-  // usando POO para pegar getAll da classe GetAll se servises
+  // usando POO para pegar getAll da classe GetAll se services
   public static async getAllClubs() {
     return GetAll.getAll();
   }
