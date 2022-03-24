a9b9ce5a083a116fc5281a97ee62fb3db1f47c91
Filipe <filipecandido123@hotmail.com>
2022-03-24 01:25:57 -0300

    feat: adicionado no services/matchs metodo que verifica qual partida esta em progresso

--- app/backend/src/services/Matchs.ts
+++ app/backend/src/services/Matchs.ts
@@ -7,7 +7,25 @@ class MatchService {
       include: [
         { model: Clubs,
           as: 'homeClub',
-          attributes: { exclude: ['id'] } },
+          attributes: { exclude: ['id'] },
+        },
+        { model: Clubs,
+          as: 'awayClub',
+          attributes: { exclude: ['id'] },
+        },
+      ],
+    });
+    return { code: 200, data: matchs };
+  }
+
+  static async getMatchsInProgress(q: string) {
+    const matchs = await Matchs.findAll({
+      where: { in_progress: q },
+      include: [
+        { model: Clubs,
+          as: 'homeClub',
+          attributes: { exclude: ['id'] },
+        },
 
         { model: Clubs,
           as: 'awayClub',
@@ -15,6 +33,7 @@ class MatchService {
         },
       ],
     });
+
     return { code: 200, data: matchs };
   }
 }
