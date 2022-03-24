3912a127a46745ec02485772a94e53f6a5932ad1
Filipe <filipecandido123@hotmail.com>
2022-03-23 23:50:27 -0300

    feat: adicionado em services/clubs metodo que recupera id de clubs

--- app/backend/src/services/Clubs.ts
+++ app/backend/src/services/Clubs.ts
@@ -5,6 +5,11 @@ class ClubService {
     const clubs = await Clubs.findAll();
     return { code: 200, data: clubs };
   }
+
+  static async getByIdClub(id: number) {
+    const club = await Clubs.findOne({ where: { id } });
+    return { code: 200, data: club };
+  }
 }
 
 export default ClubService;
