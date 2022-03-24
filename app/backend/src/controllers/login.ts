9a117481921df4eae2117be3aeb2f84a8054723c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-24 09:04:04 -0300

    refactor: use new interface

--- app/backend/src/controllers/login.ts
+++ app/backend/src/controllers/login.ts
@@ -1,10 +1,10 @@
 import { RequestHandler, Response, NextFunction } from 'express';
 import { User } from '../database/models';
-import { IRequest } from '../utils/interfaces';
+import { IRequest, ILogin } from '../utils/interfaces';
 import * as service from '../services/login';
 
 export const login: RequestHandler = (req, res, next) => service
-  .login(req.body as User)
+  .login(req.body as ILogin)
   .then(({ user, token }) => res.status(200).json({ user, token }))
   .catch(next);
 
