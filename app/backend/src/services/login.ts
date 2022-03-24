2758816881f0bc77702f47366fa53f1ced322f49
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-24 12:43:21 -0300

    fix: remove password for real

--- app/backend/src/services/login.ts
+++ app/backend/src/services/login.ts
@@ -7,14 +7,16 @@ import HttpException from '../classes/httpException';
 export const login = async (loginData: ILogin) => {
   const { email, password } = loginData;
   if (!email || !password) throw new HttpException(401, 'All fields must be filled');
-  const user = await User.findOne({ where: { email } });
-  if (!user) throw new HttpException(401, 'Incorrect email or password');
-  if (!compareSync(password, user.password)) {
+  const userData = await User.findOne({ where: { email } });
+  if (!userData) throw new HttpException(401, 'Incorrect email or password');
+  if (!compareSync(password, userData.password)) {
     throw new HttpException(401, 'Incorrect email or password');
   }
-  const { password: notUse, ...userData } = user;
+  const { id } = userData;
+  const user = await User.findByPk(id, { attributes: { exclude: ['password'] } }) as User;
   const token = createToken(user);
-  return { user: userData, token };
+  console.log(user);
+  return { user, token };
 };
 
 export const validate = async ({ email }: ILogin) => User
