3f8ae42ccb1e6bf895f8245b8eb6c3e0fbe0aad9
Filipe <filipecandido123@hotmail.com>
2022-03-23 23:51:18 -0300

    feat: adicionado em controller/clubs metodo que envia output do id de clubs

--- app/backend/src/controllers/Clubs.ts
+++ app/backend/src/controllers/Clubs.ts
@@ -6,6 +6,12 @@ class ClubsController {
     const { code, data } = await ClubService.getClubs();
     res.status(code).json(data);
   }
+
+  static async getByIdClub(req: Request, res: Response) {
+    const { id } = req.params;
+    const { code, data } = await ClubService.getByIdClub(Number(id));
+    res.status(code).json(data);
+  }
 }
 
 export default ClubsController;
