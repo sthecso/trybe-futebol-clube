a8b0d5f5c21c37b54fe6d350ea352dfbfc3c0fab
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-24 15:05:54 -0300

    fix: change error label into message

--- app/backend/src/middlewares/errorHandler.ts
+++ app/backend/src/middlewares/errorHandler.ts
@@ -2,8 +2,8 @@ import { ErrorRequestHandler } from 'express';
 
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   const status = err.status || 500;
-  const error = err.message || 'Internal error';
-  return res.status(status).send({ error });
+  const message = err.message || 'Internal error';
+  return res.status(status).send({ message });
 };
 
 export default errorHandler;
