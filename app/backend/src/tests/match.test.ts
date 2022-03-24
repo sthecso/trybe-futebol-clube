40f5276225ed6710a61d1cd77457fe74f7d8bb00
Filipe <filipecandido123@hotmail.com>
2022-03-23 23:54:38 -0300

    feat: adicionado teste de integraçao no endpoint /matchs

---
+++ app/backend/src/tests/match.test.ts
@@ -0,0 +1,38 @@
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+import { mockMatchs } from './mocks/mockMatchs';
+import { app } from '../app';
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('testa o endpoint /matchs', () => {
+  describe('caso o endpoint /matchs com o metodo getMatchs', async () => {
+    describe('retorna com sucesso', async () => {
+
+      let chaiHttpResponse: Response;
+
+      it('e uma resposta 200', async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .get('/matchs')
+        .send(mockMatchs)
+        .then((res) => {
+          expect(res.status).to.be.equal(200);
+        }) as Response;
+      });
+
+      it('e um array de objetos', async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .get('/matchs')
+        .send(mockMatchs)
+        .then((res) => {
+          expect(res.body).to.be.a('array') && expect(res.body[0]).to.be.a('object');
+        }) as Response;
+      });
+    })
+  })
+})
