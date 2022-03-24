01783f7beca1ade90bd4d9e56699899a93089fb9
Filipe <filipecandido123@hotmail.com>
2022-03-23 23:58:24 -0300

    feat: criado mock de matchs para o test de matchs

---
+++ app/backend/src/tests/mocks/mockMatchs.ts
@@ -0,0 +1,626 @@
+export const mockMatchs = [
+  {
+    id: 1,
+    home_team: 16,
+    home_team_goals: 1,
+    away_team: 8,
+    in_progress: false,
+    homeClub: {
+      club_name: "São Paulo"
+    },
+    awayClub: {
+      club_name: "Grêmio"
+    }
+  },
+  {
+    id: 2,
+    home_team: 9,
+    home_team_goals: 1,
+    away_team: 14,
+    in_progress: false,
+    homeClub: {
+      club_name: "Internacional"
+    },
+    awayClub: {
+      club_name: "Santos"
+    }
+  },
+  {
+    id: 3,
+    home_team: 4,
+    home_team_goals: 3,
+    away_team: 11,
+    in_progress: false,
+    homeClub: {
+      club_name: "Corinthians"
+    },
+    awayClub: {
+      club_name: "Napoli-SC"
+    }
+  },
+  {
+    id: 4,
+    home_team: 3,
+    home_team_goals: 0,
+    away_team: 2,
+    in_progress: false,
+    homeClub: {
+      club_name: "Botafogo"
+    },
+    awayClub: {
+      club_name: "Bahia"
+    }
+  },
+  {
+    id: 5,
+    home_team: 7,
+    home_team_goals: 1,
+    away_team: 10,
+    in_progress: false,
+    homeClub: {
+      club_name: "Flamengo"
+    },
+    awayClub: {
+      club_name: "Minas Brasília"
+    }
+  },
+  {
+    id: 6,
+    home_team: 5,
+    home_team_goals: 1,
+    away_team: 13,
+    in_progress: false,
+    homeClub: {
+      club_name: "Cruzeiro"
+    },
+    awayClub: {
+      club_name: "Real Brasília"
+    }
+  },
+  {
+    id: 7,
+    home_team: 12,
+    home_team_goals: 2,
+    away_team: 6,
+    in_progress: false,
+    homeClub: {
+      club_name: "Palmeiras"
+    },
+    awayClub: {
+      club_name: "Ferroviária"
+    }
+  },
+  {
+    id: 8,
+    home_team: 15,
+    home_team_goals: 0,
+    away_team: 1,
+    in_progress: false,
+    homeClub: {
+      club_name: "São José-SP"
+    },
+    awayClub: {
+      club_name: "Avaí/Kindermann"
+    }
+  },
+  {
+    id: 9,
+    home_team: 1,
+    home_team_goals: 0,
+    away_team: 12,
+    in_progress: false,
+    homeClub: {
+      club_name: "Avaí/Kindermann"
+    },
+    awayClub: {
+      club_name: "Palmeiras"
+    }
+  },
+  {
+    id: 10,
+    home_team: 2,
+    home_team_goals: 0,
+    away_team: 9,
+    in_progress: false,
+    homeClub: {
+      club_name: "Bahia"
+    },
+    awayClub: {
+      club_name: "Internacional"
+    }
+  },
+  {
+    id: 11,
+    home_team: 13,
+    home_team_goals: 1,
+    away_team: 3,
+    in_progress: false,
+    homeClub: {
+      club_name: "Real Brasília"
+    },
+    awayClub: {
+      club_name: "Botafogo"
+    }
+  },
+  {
+    id: 12,
+    home_team: 6,
+    home_team_goals: 0,
+    away_team: 4,
+    in_progress: false,
+    homeClub: {
+      club_name: "Ferroviária"
+    },
+    awayClub: {
+      club_name: "Corinthians"
+    }
+  },
+  {
+    id: 13,
+    home_team: 8,
+    home_team_goals: 2,
+    away_team: 5,
+    in_progress: false,
+    homeClub: {
+      club_name: "Grêmio"
+    },
+    awayClub: {
+      club_name: "Cruzeiro"
+    }
+  },
+  {
+    id: 14,
+    home_team: 14,
+    home_team_goals: 2,
+    away_team: 16,
+    in_progress: false,
+    homeClub: {
+      club_name: "Santos"
+    },
+    awayClub: {
+      club_name: "São Paulo"
+    }
+  },
+  {
+    id: 15,
+    home_team: 10,
+    home_team_goals: 0,
+    away_team: 15,
+    in_progress: false,
+    homeClub: {
+      club_name: "Minas Brasília"
+    },
+    awayClub: {
+      club_name: "São José-SP"
+    }
+  },
+  {
+    id: 16,
+    home_team: 11,
+    home_team_goals: 0,
+    away_team: 7,
+    in_progress: false,
+    homeClub: {
+      club_name: "Napoli-SC"
+    },
+    awayClub: {
+      club_name: "Flamengo"
+    }
+  },
+  {
+    id: 17,
+    home_team: 1,
+    home_team_goals: 2,
+    away_team: 8,
+    in_progress: false,
+    homeClub: {
+      club_name: "Avaí/Kindermann"
+    },
+    awayClub: {
+      club_name: "Grêmio"
+    }
+  },
+  {
+    id: 18,
+    home_team: 12,
+    home_team_goals: 4,
+    away_team: 5,
+    in_progress: false,
+    homeClub: {
+      club_name: "Palmeiras"
+    },
+    awayClub: {
+      club_name: "Cruzeiro"
+    }
+  },
+  {
+    id: 19,
+    home_team: 11,
+    home_team_goals: 2,
+    away_team: 2,
+    in_progress: false,
+    homeClub: {
+      club_name: "Napoli-SC"
+    },
+    awayClub: {
+      club_name: "Bahia"
+    }
+  },
+  {
+    id: 20,
+    home_team: 7,
+    home_team_goals: 0,
+    away_team: 9,
+    in_progress: false,
+    homeClub: {
+      club_name: "Flamengo"
+    },
+    awayClub: {
+      club_name: "Internacional"
+    }
+  },
+  {
+    id: 21,
+    home_team: 6,
+    home_team_goals: 3,
+    away_team: 13,
+    in_progress: false,
+    homeClub: {
+      club_name: "Ferroviária"
+    },
+    awayClub: {
+      club_name: "Real Brasília"
+    }
+  },
+  {
+    id: 22,
+    home_team: 4,
+    home_team_goals: 3,
+    away_team: 3,
+    in_progress: false,
+    homeClub: {
+      club_name: "Corinthians"
+    },
+    awayClub: {
+      club_name: "Botafogo"
+    }
+  },
+  {
+    id: 23,
+    home_team: 15,
+    home_team_goals: 2,
+    away_team: 16,
+    in_progress: false,
+    homeClub: {
+      club_name: "São José-SP"
+    },
+    awayClub: {
+      club_name: "São Paulo"
+    }
+  },
+  {
+    id: 24,
+    home_team: 10,
+    home_team_goals: 2,
+    away_team: 14,
+    in_progress: false,
+    homeClub: {
+      club_name: "Minas Brasília"
+    },
+    awayClub: {
+      club_name: "Santos"
+    }
+  },
+  {
+    id: 25,
+    home_team: 2,
+    home_team_goals: 0,
+    away_team: 6,
+    in_progress: false,
+    homeClub: {
+      club_name: "Bahia"
+    },
+    awayClub: {
+      club_name: "Ferroviária"
+    }
+  },
+  {
+    id: 26,
+    home_team: 13,
+    home_team_goals: 1,
+    away_team: 1,
+    in_progress: false,
+    homeClub: {
+      club_name: "Real Brasília"
+    },
+    awayClub: {
+      club_name: "Avaí/Kindermann"
+    }
+  },
+  {
+    id: 27,
+    home_team: 5,
+    home_team_goals: 1,
+    away_team: 15,
+    in_progress: false,
+    homeClub: {
+      club_name: "Cruzeiro"
+    },
+    awayClub: {
+      club_name: "São José-SP"
+    }
+  },
+  {
+    id: 28,
+    home_team: 16,
+    home_team_goals: 3,
+    away_team: 7,
+    in_progress: false,
+    homeClub: {
+      club_name: "São Paulo"
+    },
+    awayClub: {
+      club_name: "Flamengo"
+    }
+  },
+  {
+    id: 29,
+    home_team: 9,
+    home_team_goals: 0,
+    away_team: 4,
+    in_progress: false,
+    homeClub: {
+      club_name: "Internacional"
+    },
+    awayClub: {
+      club_name: "Corinthians"
+    }
+  },
+  {
+    id: 30,
+    home_team: 3,
+    home_team_goals: 0,
+    away_team: 12,
+    in_progress: false,
+    homeClub: {
+      club_name: "Botafogo"
+    },
+    awayClub: {
+      club_name: "Palmeiras"
+    }
+  },
+  {
+    id: 31,
+    home_team: 8,
+    home_team_goals: 2,
+    away_team: 10,
+    in_progress: false,
+    homeClub: {
+      club_name: "Grêmio"
+    },
+    awayClub: {
+      club_name: "Minas Brasília"
+    }
+  },
+  {
+    id: 32,
+    home_team: 14,
+    home_team_goals: 5,
+    away_team: 11,
+    in_progress: false,
+    homeClub: {
+      club_name: "Santos"
+    },
+    awayClub: {
+      club_name: "Napoli-SC"
+    }
+  },
+  {
+    id: 33,
+    home_team: 1,
+    home_team_goals: 1,
+    away_team: 16,
+    in_progress: false,
+    homeClub: {
+      club_name: "Avaí/Kindermann"
+    },
+    awayClub: {
+      club_name: "São Paulo"
+    }
+  },
+  {
+    id: 34,
+    home_team: 9,
+    home_team_goals: 3,
+    away_team: 6,
+    in_progress: false,
+    homeClub: {
+      club_name: "Internacional"
+    },
+    awayClub: {
+      club_name: "Ferroviária"
+    }
+  },
+  {
+    id: 35,
+    home_team: 10,
+    home_team_goals: 1,
+    away_team: 5,
+    in_progress: false,
+    homeClub: {
+      club_name: "Minas Brasília"
+    },
+    awayClub: {
+      club_name: "Cruzeiro"
+    }
+  },
+  {
+    id: 36,
+    home_team: 2,
+    home_team_goals: 0,
+    away_team: 7,
+    in_progress: false,
+    homeClub: {
+      club_name: "Bahia"
+    },
+    awayClub: {
+      club_name: "Flamengo"
+    }
+  },
+  {
+    id: 37,
+    home_team: 15,
+    home_team_goals: 0,
+    away_team: 13,
+    in_progress: false,
+    homeClub: {
+      club_name: "São José-SP"
+    },
+    awayClub: {
+      club_name: "Real Brasília"
+    }
+  },
+  {
+    id: 38,
+    home_team: 14,
+    home_team_goals: 2,
+    away_team: 4,
+    in_progress: false,
+    homeClub: {
+      club_name: "Santos"
+    },
+    awayClub: {
+      club_name: "Corinthians"
+    }
+  },
+  {
+    id: 39,
+    home_team: 3,
+    home_team_goals: 2,
+    away_team: 11,
+    in_progress: false,
+    homeClub: {
+      club_name: "Botafogo"
+    },
+    awayClub: {
+      club_name: "Napoli-SC"
+    }
+  },
+  {
+    id: 40,
+    home_team: 12,
+    home_team_goals: 4,
+    away_team: 8,
+    in_progress: false,
+    homeClub: {
+      club_name: "Palmeiras"
+    },
+    awayClub: {
+      club_name: "Grêmio"
+    }
+  },
+  {
+    id: 41,
+    home_team: 16,
+    home_team_goals: 2,
+    away_team: 9,
+    in_progress: true,
+    homeClub: {
+      club_name: "São Paulo"
+    },
+    awayClub: {
+      club_name: "Internacional"
+    }
+  },
+  {
+    id: 42,
+    home_team: 6,
+    home_team_goals: 1,
+    away_team: 1,
+    in_progress: true,
+    homeClub: {
+      club_name: "Ferroviária"
+    },
+    awayClub: {
+      club_name: "Avaí/Kindermann"
+    }
+  },
+  {
+    id: 43,
+    home_team: 11,
+    home_team_goals: 0,
+    away_team: 10,
+    in_progress: true,
+    homeClub: {
+      club_name: "Napoli-SC"
+    },
+    awayClub: {
+      club_name: "Minas Brasília"
+    }
+  },
+  {
+    id: 44,
+    home_team: 7,
+    home_team_goals: 2,
+    away_team: 15,
+    in_progress: true,
+    homeClub: {
+      club_name: "Flamengo"
+    },
+    awayClub: {
+      club_name: "São José-SP"
+    }
+  },
+  {
+    id: 45,
+    home_team: 5,
+    home_team_goals: 1,
+    away_team: 3,
+    in_progress: true,
+    homeClub: {
+      club_name: "Cruzeiro"
+    },
+    awayClub: {
+      club_name: "Botafogo"
+    }
+  },
+  {
+    id: 46,
+    home_team: 4,
+    home_team_goals: 1,
+    away_team: 12,
+    in_progress: true,
+    homeClub: {
+      club_name: "Corinthians"
+    },
+    awayClub: {
+      club_name: "Palmeiras"
+    }
+  },
+  {
+    id: 47,
+    home_team: 8,
+    home_team_goals: 1,
+    away_team: 14,
+    in_progress: true,
+    homeClub: {
+      club_name: "Grêmio"
+    },
+    awayClub: {
+      club_name: "Santos"
+    }
+  },
+  {
+    id: 48,
+    home_team: 13,
+    home_team_goals: 1,
+    away_team: 2,
+    in_progress: true,
+    homeClub: {
+      club_name: "Real Brasília"
+    },
+    awayClub: {
+      club_name: "Bahia"
+    }
+  }
+]
