7320ebb06b8a72cc14693b410e438ba469cbfb00
Felipe Lima <felipe_lima10@icloud.com>
2022-03-24 16:01:29 -0300

    test: add get clubs success and get clube by id success

--- app/backend/src/tests/clubes.test.ts
+++ app/backend/src/tests/clubes.test.ts
@@ -13,4 +13,23 @@ const { expect } = chai;
 describe('testing "/clubes" route', () => {
   let chaiHttpResponse: Response;
 
+
+  it('a) if get clubs was successful', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/clubes')
+
+    expect(chaiHttpResponse).to.have.status(StatusCodes.OK);
+    expect(chaiHttpResponse.body).to.be.an('array');
+  });
+
+  it('b) if get clube by id was successful', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/clubes/1')
+  
+    expect(chaiHttpResponse).to.have.status(StatusCodes.OK);
+    expect(chaiHttpResponse.body).to.have.property('id');
+    expect(chaiHttpResponse.body).to.have.property('clubName');
+  })
 });
