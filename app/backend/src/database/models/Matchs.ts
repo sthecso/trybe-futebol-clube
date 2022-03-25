35b4ee17eab263dc2fce3037a7eec3374df5d6d9
sthecso <sthecarol0411@gmail.com>
2022-03-25 18:35:07 -0300

    feature: create setup inicial with sequelize and docker configurations

---
+++ app/backend/src/database/models/Matchs.ts
@@ -0,0 +1,47 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Clubs from './Clubs';
+
+class Matchs extends Model {}
+
+Matchs.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  home_team: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  home_team_goals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  away_team: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  away_team_goals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  in_progress: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  }
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Matchs', 
+  timestamps: false,
+});
+
+Clubs.belongsTo(Matchs, { foreignKey: 'home_team', as: 'homeTeam' });
+Clubs.belongsTo(Matchs, { foreignKey: 'away_team', as: 'awayTeam' });
+
+Matchs.hasMany(Clubs, { foreignKey: 'home_team', as: 'homeTeam' });
+Matchs.hasMany(Clubs, { foreignKey: 'away_team', as: 'awayTeam' });
+
+export default Matchs;
---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,35 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Users extends Model {}
+
+Users.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  username: {
+    type: DataTypes.STRING
+  },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING
+  }
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Users', 
+  timestamps: false,
+});
+
+export default Users;
