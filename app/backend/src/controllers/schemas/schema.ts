738a6a444598c34add5ae0ffa461d00db37e540e
sthecso <sthecarol0411@gmail.com>
2022-03-29 17:42:51 -0300

    add message to string.empty in schema

--- app/backend/src/controllers/schemas/schema.ts
+++ app/backend/src/controllers/schemas/schema.ts
@@ -2,10 +2,13 @@ import * as Joi from 'joi';
 
 const validateLogin: Joi.Schema = Joi.object({
   email: Joi.string().required().messages({
+    // eslint-disable-next-line sonarjs/no-duplicate-string
     'any.required': 'All fields must be filled',
+    'string.empty': 'All fields must be filled',
   }),
   password: Joi.string().min(6).required().messages({
     'any.required': 'All fields must be filled',
+    'string.empty': 'All fields must be filled',
   }),
 });
 
