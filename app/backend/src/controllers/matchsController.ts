1572d40472ff16315f59edb72c3da3beb755d531
sthecso <sthecarol0411@gmail.com>
2022-03-31 03:48:24 -0300

    add 18 and 19 partial

--- app/backend/src/controllers/matchsController.ts
+++ app/backend/src/controllers/matchsController.ts
@@ -4,6 +4,8 @@ import matchsService from '../services/matchsService';
 class ClubsController {
   public path = '/matchs';
 
+  public pathWithQuery = '/matchs?inProgress=false';
+
   public Service = matchsService;
 
   public router = express.Router();
@@ -14,6 +16,7 @@ class ClubsController {
 
   public initializeRoutes() {
     this.router.get(this.path, this.getAll);
+    this.router.get(this.pathWithQuery, this.getByProgress);
   }
 
   public getAll = async (
@@ -24,6 +27,21 @@ class ClubsController {
     try {
       const matchs = await this.Service.getAll();
       res.status(200).json(matchs);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getByProgress = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    try {
+      const { inProgress } = req.query;
+      const query = await JSON.parse(await JSON.parse(await JSON.stringify(inProgress)));
+      const match = await this.Service.getByProgress(query);
+      res.status(200).json(match);
     } catch (error) {
       next(error);
     }
--- app/backend/src/services/matchsService.ts
+++ app/backend/src/services/matchsService.ts
@@ -1,3 +1,5 @@
+import { ParsedQs } from 'qs';
+import Clubs from '../database/models/Clubs';
 import Matchs from '../database/models/Matchs';
 // import { IMatchs } from '../interfaces/IMatchs';
 
@@ -7,6 +9,21 @@ class MatchsService {
   public getAll = async () => {
     const matchs = await this._MatchsModel.findAll();
     return matchs;
+  };
+
+  public getByProgress = async (
+    inProgress: string | ParsedQs | string[] | ParsedQs[] | undefined,
+  ) => {
+    const match = await this._MatchsModel.findAll({
+      where: {
+        inProgress,
+      },
+      include: {
+        model: Clubs,
+        attributes: ['clubName'],
+      },
+    });
+    return match;
   };
 }
 
